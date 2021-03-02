83e2d030555038a75f5b4bf046d71614b1b7bbd1
Liam Huber <liam.huber@gmail.com>
2021-03-02 22:16:45 +0100

    Move helper out of class and hide it
    I didn't like the pycharm highlighting of the first argument as though it were self, since in our use it's not self.

--- pyiron_atomistics/atomistics/structure/factories/ase.py
+++ pyiron_atomistics/atomistics/structure/factories/ase.py
@@ -24,41 +24,42 @@ __date__ = "Feb 26, 2021"
 s = Settings()
 
 
+def _ase_wrapped_doc(f_name, chain):
+    return """
+    Returns an ASE's {} result, wrapped as a `pyiron_atomistics.atomstic.structure.atoms.Atoms` object.
+
+    {} docstring:
+
+    """.format(f_name, chain)
+
+
 class AseFactory:
-    def ase_wrapped_doc(f_name, chain):
-        return """
-        Returns an ASE's {} result, wrapped as a `pyiron_atomistics.atomstic.structure.atoms.Atoms` object.
-
-        {} docstring:
-
-        """.format(f_name, chain)
-
     @wraps(ase_bulk)
     def bulk(self, *args, **kwargs):
         s.publication_add(publication_ase())
         return ase_to_pyiron(ase_bulk(*args, **kwargs))
-    bulk.__doc__ = ase_wrapped_doc('bulk','ase.build.bulk') + bulk.__doc__
+    bulk.__doc__ = _ase_wrapped_doc('bulk', 'ase.build.bulk') + bulk.__doc__
 
     @wraps(ase_cut)
     def cut(self, *args, **kwargs):
         s.publication_add(publication_ase())
         return ase_cut(*args, **kwargs)
-    cut.__doc__ = ase_wrapped_doc('cut','ase.build.cut') + cut.__doc__
+    cut.__doc__ = _ase_wrapped_doc('cut', 'ase.build.cut') + cut.__doc__
 
     @wraps(ase_stack)
     def stack(self, *args, **kwargs):
         s.publication_add(publication_ase())
         return ase_stack(*args, **kwargs)
-    stack.__doc__ = ase_wrapped_doc('stack', 'ase.build.stack') + stack.__doc__
+    stack.__doc__ = _ase_wrapped_doc('stack', 'ase.build.stack') + stack.__doc__
 
     @wraps(ase_crystal)
     def crystal(self, *args, **kwargs):
         s.publication_add(publication_ase())
         return ase_to_pyiron(ase_crystal(*args, **kwargs))
-    crystal.__doc__ = ase_wrapped_doc('crystal', 'ase.spacegroup.crystal') + crystal.__doc__
+    crystal.__doc__ = _ase_wrapped_doc('crystal', 'ase.spacegroup.crystal') + crystal.__doc__
 
     @wraps(ase_read)
     def read(self, *args, **kwargs):
         s.publication_add(publication_ase())
         return ase_to_pyiron(ase_read(*args, **kwargs))
-    read.__doc__ = ase_wrapped_doc('read', 'ase.io.read') + read.__doc__
+    read.__doc__ = _ase_wrapped_doc('read', 'ase.io.read') + read.__doc__
