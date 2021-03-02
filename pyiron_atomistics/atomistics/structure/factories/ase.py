ed49eddbf9c36d671506ab0e94ef6860126993aa
Ali Zendegani <zendegani@users.noreply.github.com>
2021-03-02 14:29:09 +0100

    DRY: Add ase_wrapped_doc method to shorten the implementation of prepending the docstring

--- pyiron_atomistics/atomistics/structure/factories/ase.py
+++ pyiron_atomistics/atomistics/structure/factories/ase.py
@@ -15,7 +15,7 @@ __copyright__ = (
     "Copyright 2021, Max-Planck-Institut für Eisenforschung GmbH - "
     "Computational Materials Design (CM) Department"
 )
-__version__ = "1.0"
+__version__ = "1.0.1"
 __maintainer__ = "Liam Huber"
 __email__ = "huber@mpie.de"
 __status__ = "production"
@@ -25,57 +25,40 @@ s = Settings()
 
 
 class AseFactory:
+    def ase_wrapped_doc(f_name, chain):
+        return """
+        Returns an ASE's {} result, wrapped as a `pyiron_atomistics.atomstic.structure.atoms.Atoms` object.
+
+        {} docstring:
+
+        """.format(f_name, chain)
+
     @wraps(ase_bulk)
     def bulk(self, *args, **kwargs):
         s.publication_add(publication_ase())
         return ase_bulk(*args, **kwargs)
-    bulk.__doc__ = """
-        Returns an ASE's bulk result, wrapped as a `pyiron_atomistics.atomstic.structure.atoms.Atoms` object.
-
-        ase.build.bulk docstring:
-
-        """ + bulk.__doc__ 
+    bulk.__doc__ = ase_wrapped_doc('bulk','ase.build.bulk') + bulk.__doc__
 
     @wraps(ase_cut)
     def cut(self, *args, **kwargs):
         s.publication_add(publication_ase())
         return ase_cut(*args, **kwargs)
-    cut.__doc__ = """
-        Returns an ASE's cut result, wrapped as a `pyiron_atomistics.atomstic.structure.atoms.Atoms` object.
-
-        ase.build.cut docstring:
-
-        """ + cut.__doc__
+    cut.__doc__ = ase_wrapped_doc('cut','ase.build.cut') + cut.__doc__
 
     @wraps(ase_stack)
     def stack(self, *args, **kwargs):
         s.publication_add(publication_ase())
         return ase_stack(*args, **kwargs)
-    stack.__doc__ = """
-        Returns an ASE's stack result, wrapped as a `pyiron_atomistics.atomstic.structure.atoms.Atoms` object.
-
-        ase.build.stack docstring:
-
-        """ + stack.__doc__ 
+    stack.__doc__ = ase_wrapped_doc('stack', 'ase.build.stack') + stack.__doc__
 
     @wraps(ase_crystal)
     def crystal(self, *args, **kwargs):
         s.publication_add(publication_ase())
         return ase_to_pyiron(ase_crystal(*args, **kwargs))
-    crystal.__doc__ = """
-        Returns an ASE's crystal result, wrapped as a `pyiron_atomistics.atomstic.structure.atoms.Atoms` object.
-
-        ase.spacegroup.crystal docstring:
-
-        """ + crystal.__doc__ 
+    crystal.__doc__ = ase_wrapped_doc('crystal', 'ase.spacegroup.crystal') + crystal.__doc__
 
     @wraps(ase_read)
     def read(self, *args, **kwargs):
         s.publication_add(publication_ase())
         return ase_to_pyiron(ase_read(*args, **kwargs))
-    read.__doc__ = """
-        Returns an ASE's read result, wrapped as a `pyiron_atomistics.atomstic.structure.atoms.Atoms` object.
-
-        ase.io.read docstring:
-        
-        """ + read.__doc__  
+    read.__doc__ = ase_wrapped_doc('read', 'ase.io.read') + read.__doc__
