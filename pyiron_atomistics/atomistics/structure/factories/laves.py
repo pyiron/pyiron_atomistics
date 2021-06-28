3aa70d1c8bd1aac39d608f11485bae57f38b91a4
Liam Huber <liam.huber@gmail.com>
2021-06-28 16:40:59 +0200

    Add C15 creator

--- pyiron_atomistics/atomistics/structure/factories/laves.py
+++ pyiron_atomistics/atomistics/structure/factories/laves.py
@@ -3,6 +3,7 @@
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
 from pyiron_atomistics.atomistics.structure.factories.ase import AseFactory
+import numpy as np
 
 __author__ = "Liam Huber"
 __copyright__ = (
@@ -24,8 +25,40 @@ class LavesFactory:
     def C14(self):
         raise NotImplementedError
 
-    def C15(self):
-        raise NotImplementedError
+    def C15(self, element_a, element_b, a=None):
+        """
+        Builds a cubic $A B_2$ C15 Laves phase cell.
+
+        Args:
+            element_a (str): The chemical symbol for the A element.
+            element_b (str): The chemical symbol for the B element.
+            a (float): The lattice constant. (Default is None, which uses the default nearest-neighbour distance for
+                the A-type element.)
+
+        Returns:
+            (Atoms): The C15 unit cell.
+        """
+        if a is None:
+            a = self._bulk(name=element_a).get_neighbors(num_neighbors=1).distances[0, 0] * (4 / np.sqrt(3))
+
+        structure = self._bulk(name=element_a, crystalstructure='diamond', a=a, cubic=True)
+        secondary = self._bulk(name=element_b, crystalstructure='diamond', a=a, cubic=True)
+        evens = secondary[[i for i in range(len(secondary)) if i % 2 == 0]]
+        odds = secondary[[i for i in range(len(secondary)) if i % 2 == 1]]
+
+        for shifts, sublattice in zip(
+                0.125 * np.array([
+                    [[-1, 3, -1], [-1, -3, 1]],
+                    [[-1, -3, -1], [-1, 3, 1]]
+                ]),
+                [odds, evens]
+        ):
+            for shift in shifts:
+                sub = sublattice.copy()
+                sub.positions += np.dot(sub.cell.array, shift.T)
+                structure += sub
+
+        return structure
 
     def C36(self):
         raise NotImplementedError
