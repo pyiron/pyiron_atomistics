994f34b0f2383f73702cbdc696f03c015dc03023
Sam Waseda <o.waseda@mpie.de>
2021-07-14 17:41:22 +0200

    implement `group_points_by_symmetry`

--- pyiron_atomistics/atomistics/structure/analyse.py
+++ pyiron_atomistics/atomistics/structure/analyse.py
@@ -577,7 +577,8 @@ class Analyse:
         use_elements=True,
         symprec=1e-5,
         angle_tolerance=-1.0,
-        return_unique=True
+        return_unique=True,
+        epsilon=1.0e-8
     ):
         """
 
@@ -588,6 +589,7 @@ class Analyse:
             symprec (float): cf. get_symmetry()
             angle_tolerance (float): cf. get_symmetry()
             return_unique (bool): Return only points which appear once.
+            epsilon (float): displacement to add to avoid wrapping of atoms at borders
 
         Returns:
             (ndarray): array of equivalent points with respect to box symmetries
@@ -602,7 +604,8 @@ class Analyse:
         t = symmetry_operations['translations']
         x = np.einsum('jk,nj->nk', np.linalg.inv(self._structure.cell), np.atleast_2d(points))
         x = np.einsum('nxy,my->mnx', R, x)+t
-        x -= np.floor(x)
+        if any(self._structure.pbc):
+            x[:,:,self._structure.pbc] -= np.floor(x[:,:,self._structure.pbc]+epsilon)
         if not return_unique:
             return np.einsum(
                 'ji,mnj->mni', self._structure.cell, x
@@ -612,3 +615,44 @@ class Analyse:
             np.round(x, decimals=int(-np.log10(symprec))), return_index=True, axis=0
         )
         return np.einsum('ji,mj->mi', self._structure.cell, x[indices])
+
+    def group_points_by_symmetry(
+        self,
+        points,
+        use_magmoms=False,
+        use_elements=True,
+        symprec=1e-5,
+        angle_tolerance=-1.0,
+        epsilon=1.0e-8
+    ):
+        """
+
+        Group points according to the box symmetries
+
+        Args:
+            points (list/ndarray): 3d vector
+            use_magmoms (bool): cf. get_symmetry()
+            use_elements (bool): cf. get_symmetry()
+            symprec (float): cf. get_symmetry()
+            angle_tolerance (float): cf. get_symmetry()
+            epsilon (float): displacement to add to avoid wrapping of atoms at borders
+
+        Returns:
+            (ndarray): array of ID's according to their groups
+        """
+        all_points = self.get_equivalent_points(
+            points=points,
+            use_magmoms=use_magmoms,
+            use_elements=use_elements,
+            symprec=symprec,
+            angle_tolerance=angle_tolerance,
+            epsilon=epsilon,
+            return_unique=False
+        )
+        decimals = int(-np.log10(symprec))
+        _, inverse = np.unique(
+            np.round(all_points.reshape(-1, 3), decimals=decimals), axis=0, return_inverse=True
+        )
+        inverse = inverse.reshape(all_points.shape[:-1][::-1])
+        indices = np.min(inverse, axis=1)
+        return np.unique(indices, return_inverse=True)[1]
