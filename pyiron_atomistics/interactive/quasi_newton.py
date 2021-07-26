148e143751967a7923d2f92fef9e144a121a689a
Sam Waseda <o.waseda@mpie.de>
2021-07-26 17:26:37 +0200

    put run_qn

--- pyiron_atomistics/interactive/quasi_newton.py
+++ pyiron_atomistics/interactive/quasi_newton.py
@@ -1,8 +1,13 @@
 from scipy import sparse
 import numpy as np
+from pyiron_base import DataContainer
+from pyiron_atomistics.atomistics.job.interactivewrapper import (
+    InteractiveWrapper,
+    ReferenceJobOutput,
+)
 
 
-class QuasiNewton:
+class QuasiNewtonInteractive:
     def __init__(
         self,
         structure,
@@ -18,6 +23,7 @@ class QuasiNewton:
         self._eigenvectors = None
         self.g_old = None
         self._initialize_hessian(
+            structure=structure,
             starting_h=starting_h,
             diffusion_id=diffusion_id,
             diffusion_direction=diffusion_direction
@@ -29,7 +35,9 @@ class QuasiNewton:
         if self.use_eigenvalues and self.regularization==0:
             raise ValueError('Regularization must be larger than 0 when eigenvalues are used')
 
-    def _initialize_hessian(self, starting_h=10, diffusion_id=None, diffusion_direction=None):
+    def _initialize_hessian(
+        self, structure, starting_h=10, diffusion_id=None, diffusion_direction=None
+    ):
         self.hessian = starting_h*np.eye(np.prod(structure.positions.shape))
         if diffusion_id is not None and diffusion_direction is not None:
             v = np.zeros_like(structure.positions)
@@ -79,8 +87,8 @@ class QuasiNewton:
             self._calc_eig()
         return self._eigenvectors
 
-    def get_dx(self, g):
-        self.update_hessian(g)
+    def get_dx(self, g, threshold=1e-4, mode='PSB'):
+        self.update_hessian(g, threshold=threshold, mode=mode)
         self.dx = -np.einsum('ij,j->i', self.inv_hessian, g.flatten()).reshape(-1, 3)
         return self.dx
     
@@ -106,3 +114,124 @@ class QuasiNewton:
 
     def get_dg(self, g):
         return g-self.g_old
+
+def run_qn(
+    job,
+    mode='PSB',
+    ionic_steps=100,
+    ionic_force_tolerance=1.0e-2,
+    ionic_energy_tolerance=0,
+    starting_h=10,
+    diffusion_id=None,
+    use_eigenvalues=True,
+    diffusion_direction=None,
+    regularization=1e-6,
+):
+    qn = QuasiNewtonInteractive(
+        structure=job.structure,
+        starting_h=starting_h,
+        diffusion_id=diffusion_id,
+        use_eigenvalues=use_eigenvalues,
+        diffusion_direction=diffusion_direction,
+        regularization=regularization,
+    )
+    job.run()
+    for _ in range(ionic_steps):
+        f = job.output.forces[-1]
+        if np.linalg.norm(f, axis=-1).max() < ionic_force_tolerance:
+            break
+        dx = qn.get_dx(-f, mode=mode)
+        job.structure.positions += dx
+        job.structure.center_coordinates_in_unit_cell()
+        job.run()
+    return qn
+
+class QuasiNewton(InteractiveWrapper):
+    def __init__(self, project, job_name):
+        super().__init__(project, job_name)
+        self.__name__ = "QuasiNewton"
+        self.__version__ = (
+            None
+        )  # Reset the version number to the executable is set automatically
+        self.input = Input()
+        self.output = Output(self)
+        self._interactive_interface = None
+
+    def _run(self):
+        run_qn(
+            job=self.ref_job,
+            mode=self.input.mode,
+            ionic_steps=self.input.ionic_steps,
+            ionic_force_tolerance=self.input.ionic_force_tolerance,
+            ionic_energy_tolerance=self.input.ionic_energy_tolerance,
+            starting_h=self.input.starting_h,
+            diffusion_id=self.input.diffusion_id,
+            use_eigenvalues=self.input.use_eigenvalues,
+            diffusion_direction=self.input.diffusion_direction,
+            regularization=self.input.regularization
+        )
+        self.collect_output()
+
+    def run_if_interactive(self):
+        self._run()
+
+    def run_if_static(self):
+        self._run()
+        self.interactive_close()
+
+    def interactive_open(self):
+        self.server.run_mode.interactive = True
+        self.ref_job.interactive_open()
+
+    def interactive_close(self):
+        self.status.collect = True
+        if self.ref_job.server.run_mode.interactive:
+            self.ref_job.interactive_close()
+        self.run()
+
+    def write_input(self):
+        pass
+
+    def to_hdf(self, hdf=None, group_name=None):
+        super().to_hdf(
+            hdf=hdf,
+            group_name=group_name
+        )
+
+    def from_hdf(self, hdf=None, group_name=None):
+        super().from_hdf(
+            hdf=hdf,
+            group_name=group_name
+        )
+
+    def collect_output(self):
+        self.output._index_lst.append(len(self.ref_job.output.energy_pot))
+
+class Input(DataContainer):
+    """
+    Args:
+        minimizer (str): minimizer to use (currently only 'CG' and 'BFGS' run
+            reliably)
+        ionic_steps (int): max number of steps
+        ionic_force_tolerance (float): maximum force tolerance
+    """
+
+    def __init__(self, input_file_name=None, table_name="input"):
+        self.mode = 'PSB'
+        self.ionic_steps = 100
+        self.ionic_force_tolerance = 1.0e-2
+        self.ionic_energy_tolerance = 0
+        self.starting_h = 10
+        self.diffusion_id = None
+        self.use_eigenvalues = True
+        self.diffusion_direction = None
+        self.regularization = 1e-6
+
+class Output(ReferenceJobOutput):
+    def __init__(self, job):
+        super().__init__(job=job)
+        self._index_lst = []
+
+    @property
+    def index_lst(self):
+        return np.asarray(self._index_lst)
