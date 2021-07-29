8db90fc2788c93cfa26e64b073326a7807801eb9
Sudarsan Surendralal <surendralal@mpie.de>
2021-07-29 10:26:08 +0200

    Update pyiron_atomistics/vasp/outcar.py
    Co-authored-by: Marvin Poul <poul@mpie.de>

--- pyiron_atomistics/vasp/outcar.py
+++ pyiron_atomistics/vasp/outcar.py
@@ -839,7 +839,7 @@ class Outcar(object):
                     if all(cbm_bool):
                         vbm_level_dict[spin].append(cbm_level_dict[spin][-1])
                     else:
-                        vbm_level_dict[spin].append(band_energy[np.abs(band_occ) >= 1e-6][-1])
+                        vbm_level_dict[spin].append(band_energy[~cbm_bool][-1])
         return np.array(fermi_level_list), np.array([val for val
                                                      in vbm_level_dict.values()]), np.array([val
                                                                                              for val in
