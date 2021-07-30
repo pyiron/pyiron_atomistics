b93d3a866ac274382bab797ad2470c905efd9b53
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-07-30 15:46:08 +0200

    Update __init__.py

--- pyiron_atomistics/__init__.py
+++ pyiron_atomistics/__init__.py
@@ -1,6 +1,7 @@
 __version__ = "0.1"
 __all__ = []
 
+
 from pyiron_atomistics.project import Project
 from pyiron_atomistics.atomistics.structure.atoms import ase_to_pyiron, pyiron_to_ase, Atoms
 from pyiron_base import Notebook, install_dialog, JOB_CLASS_DICT
