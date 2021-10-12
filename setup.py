f2c013bb57e770405cdb727e2002df684ca958d0
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-10-12 19:37:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'numpy==1.21.2',
         'pandas==1.3.3',
         'phonopy==2.11.0',
-        'pyiron_base==0.3.6',
+        'pyiron_base==0.3.7',
         'pymatgen==2022.0.14',
         'scipy==1.7.1',
         'seekpath==2.0.1',
