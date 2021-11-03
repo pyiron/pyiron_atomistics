2ad076445cd890522f88aefb168eaa4bb0f57eb2
skatnagallu <s.katnagallu@mpie.de>
2021-11-03 12:49:57 +0100

    Changes to the unittest

--- tests/atomistics/structure/test_high_index_surface.py
+++ tests/atomistics/structure/test_high_index_surface.py
@@ -4,7 +4,7 @@ from pyiron_atomistics.atomistics.structure.factory import StructureFactory
 
 class TestHighIndexSurface(unittest.TestCase):
     def test_high_index_surface(self):
-        k, s, h, slab = StructureFactory().get_high_index_surface(element='Ni', crystal_structure='fcc',
+        slab, h, s, k = StructureFactory().get_high_index_surface(element='Ni', crystal_structure='fcc',
                                                                   lattice_constant=3.526,
                                                                   terrace_orientation=[1, 1, 1],
                                                                   step_orientation=[1, 1, 0],
@@ -13,9 +13,10 @@ class TestHighIndexSurface(unittest.TestCase):
                                                                   length_terrace=3,
                                                                   length_kink=1, layers=60,
                                                                   vacuum=10)
-        self.assertEqual(h, [-13, -7, -9])
-        self.assertEqual(k, [-1, 0, 1])
-        self.assertEqual(s, [0, -1, 1])
+        self.assertEqual(len(h), 3)
+        self.assertEqual(h[0], -9)
+        self.assertEqual(len(k), 3)
+        self.assertEqual(len(s), 3)
 
 
 if __name__ == '__main__':
