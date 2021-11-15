6c6cbae4744b43280666341c595ace4b4b99303a
liamhuber <liam.huber@gmail.com>
2021-11-15 14:39:01 +0100

    Refactor: extract state management

--- tests/atomistics/structure/test_potentials.py
+++ tests/atomistics/structure/test_potentials.py
@@ -6,47 +6,72 @@ import os
 import unittest
 from pyiron_atomistics.lammps.potential import LammpsPotentialFile
 from pyiron_atomistics.vasp.potential import VaspPotential
+from pyiron_base import state
+from abc import ABC
 
 
-class TestLammpsPotentialFile(unittest.TestCase):
+class _PotentialTester(unittest.TestCase, ABC):
+    """Overrides the settings so that the tests/static directory is used as the resources, then refreshes afterwards"""
+
     @classmethod
     def setUpClass(cls):
+        state.update({'resource_paths': os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../static")})
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        state.update()
+
+
+class TestLammpsPotentialFile(_PotentialTester):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         cls.kim = LammpsPotentialFile()
-        cls.potential_path = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "../../static/lammps/potentials"
-        )
 
     def test_find(self):
-        Fe_lst = ["Fe_C_Becquart_eam", "Fe_C_Hepburn_Ackland_eam"]
-
-        self.assertEqual(sorted(list(self.kim.find("Fe")["Name"])), sorted(Fe_lst))
-        AlMg_lst = ["Al_Mg_Mendelev_eam"]
-        self.assertEqual(sorted(list(self.kim.find({"Al", "Mg"})["Name"])), AlMg_lst)
+        Fe_desired = ["Fe_C_Becquart_eam", "Fe_C_Hepburn_Ackland_eam"]
+        Fe_available = list(self.kim.find("Fe")["Name"])
+        for potl in Fe_desired:
+            self.assertIn(
+                potl, Fe_available,
+                msg=f"Failed to find {potl}, which is expected since it is in the tests/static resources"
+            )
+        self.assertIn(
+            "Al_Mg_Mendelev_eam", list(self.kim.find({"Al", "Mg"})["Name"]),
+            msg="Failed to find Al_Mg_Mendelev_eam, which is expected since it is in the tests/static resources"
+        )
 
     def test_pythonic_functions(self):
         self.assertEqual(
-            list(self.kim.find("Fe")["Name"]), list(self.kim["Fe"].list()["Name"])
+            list(self.kim.find("Fe")["Name"]), list(self.kim["Fe"].list()["Name"]),
+            msg="List conversion method failed."
         )
         self.assertEqual(
-            list(self.kim.find("Fe")["Name"]), list(self.kim.Fe.list()["Name"])
+            list(self.kim.find("Fe")["Name"]), list(self.kim.Fe.list()["Name"]),
+            msg="Element symbol attribute does not find element."
         )
         self.assertEqual(
             list(self.kim.find({"Al", "Mg"})["Name"]),
             list(self.kim["Al"]["Mg"].list()["Name"]),
+            msg="Double find not equivalent to nested dictionary access."
         )
         self.assertEqual(
             list(self.kim.find({"Al", "Mg"})["Name"]),
+            list(self.kim.Al.Mg.list()["Name"]),
+            msg="Nested attribute access failed"
+        )
+        self.assertEqual(
             list(self.kim.Mg.Al.list()["Name"]),
+            list(self.kim.Al.Mg.list()["Name"]),
+            msg="Elemental pairs should be commutative"
         )
 
 
-class TestVaspPotential(unittest.TestCase):
+class TestVaspPotential(_PotentialTester):
     @classmethod
     def setUpClass(cls):
+        super().setUpClass()
         cls.vasp = VaspPotential()
-        cls.potential_path = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "../../static/vasp/potentials"
-        )
 
     def test_find(self):
         self.assertEqual(
