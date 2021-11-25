2e8b493ebaad38b81346286e893258741ec22acb
Sam Waseda <o.waseda@mpie.de>
2021-11-25 08:44:20 +0100

    replace full output info in __str__ by chemical formula

--- pyiron_atomistics/atomistics/structure/atoms.py
+++ pyiron_atomistics/atomistics/structure/atoms.py
@@ -2031,9 +2031,6 @@ class Atoms(ASEAtoms):
     #     return len(self.indices)
 
     def __repr__(self):
-        return self.__str__()
-
-    def __str__(self):
         if len(self) == 0:
             return "[]"
         out_str = ""
@@ -2051,6 +2048,9 @@ class Atoms(ASEAtoms):
             out_str += "cell: \n"
             out_str += str(self.cell) + "\n"
         return out_str
+
+    def __str__(self):
+        return self.get_chemical_formula()
 
     def __setitem__(self, key, value):
         if isinstance(key, (int, np.integer)):
