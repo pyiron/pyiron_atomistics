09581b892fd06ed62f3228b4b5ef810eff4f0b0e
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2022-01-11 21:42:12 +0100

    Drop python3.7 support

--- setup.py
+++ setup.py
@@ -22,10 +22,9 @@ setup(
                  'Intended Audience :: Science/Research',
                  'Operating System :: OS Independent',
                  'Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.6',
-                 'Programming Language :: Python :: 3.7',
                  'Programming Language :: Python :: 3.8',
-                 'Programming Language :: Python :: 3.9'],
+                 'Programming Language :: Python :: 3.9',
+                 'Programming Language :: Python :: 3.10'],
 
     keywords='pyiron',
     packages=find_packages(exclude=["*tests*", "*docs*", "*binder*", "*conda*", "*notebooks*", "*.ci_support*"]),
