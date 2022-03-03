80ca6870c93afca6a33ddea9fbfe9acff0bbc30d
srmnitc <sarath.r.menon@gmail.com>
2022-03-03 09:43:30 +0100

    update implementation

--- pyiron_atomistics/calphy/base.py
+++ pyiron_atomistics/calphy/base.py
@@ -1,145 +1,19 @@
-import copy
-import os
-import numpy as np
-
+from pyiron_base import DataContainer
 from pyiron_atomistics.lammps.potential import LammpsPotential
-from pyiron_atomistics.lammps.structure import LammpsStructure, UnfoldingPrism
 from pyiron_atomistics.lammps.potential import LammpsPotentialFile, PotentialAvailable
 from pyiron_base import GenericJob
+from pyiron_atomistics.lammps.structure import LammpsStructure, UnfoldingPrism
 
 from calphy.input import check_and_convert_to_list
 from calphy.queuekernel import Solid, Liquid, routine_fe, routine_ts
 
-class CalphyBase(GenericJob):
-    def __init__(self, project, job_name):
-        super(CalphyBase, self).__init__(project, job_name)
-        #now calphy input parameters need to be handled; for the moment minimal is better
-        self.__name__ = "CalphyJob"
-        #things that will be taken over by pyiron
-        #element, mass, lattice, repeat, lattice-constant
-        self.structure = False
-        self.input = Input()
-        self._data = None
-        
-    @property
-    def potential(self):
-        return self.input.potential.df
-    
-    @potential.setter
-    def potential(self, potential_filename):
-        potential_db = LammpsPotentialFile()
-        potential = potential_db.find_by_name(potential_filename)
-        self.input.potential.df = potential
-        
-    def view_potentials(self):
-        if not self.structure:
-            raise ValueError("No structure set.")
-        list_of_elements = set(self.structure.get_chemical_symbols())
-        list_of_potentials = LammpsPotentialFile().find(list_of_elements)
-        if list_of_potentials is not None:
-            return list_of_potentials
-        else:
-            raise TypeError(
-                "No potentials found for this kind of structure: ",
-                str(list_of_elements),
-            )
+import copy
+import os
+import numpy as np
 
-    def write_input(self):
-        self.input.prepare_input(self.structure, 
-                                cores = self.server.cores)
-        #write structure
-        file_name = "conf.data"
-        self.input.write_structure(file_name, self.working_directory)
-        #write potentials
-        self.input.potential.copy_pot_files(self.working_directory)
-        #now reset the lattice in options
-        self.input.options["calculations"][0]["lattice"] = os.path.join(self.working_directory,
-                                                                       "conf.data")
-    def run_static(self):
-        #now we have to actually run the job;
-        if self.input.mode == "fe":
-            if self.input.options["calculations"][0]["state"] == "liquid":
-                job = Liquid(options=self.input.options, 
-                             kernel=0, 
-                             simfolder=self.working_directory)
-            else:
-                job = Solid(options=self.input.options, 
-                             kernel=0, 
-                             simfolder=self.working_directory)
-            routine_fe(job)
-            #this could be changed to an ordered dict;
-            #needs to be done on the pyscal side
-            self._data = job.report
-            del self._data['input']
-
-        elif self.input.mode == "ts":
-            if self.input.options["calculations"][0]["state"] == "liquid":
-                job = Liquid(options=self.input.options, 
-                             kernel=0, 
-                             simfolder=self.working_directory)
-            else:
-                job = Solid(options=self.input.options, 
-                             kernel=0, 
-                             simfolder=self.working_directory)
-            routine_ts(job)
-            self._data = job.report
-            del self._data['input']
-
-        self.collect_output()
-
-    def collect_output(self):
-        self.to_hdf()
-        self.collect_logs()
-        
-    def to_hdf(self, hdf=None, group_name=None):
-        super().to_hdf(hdf=hdf, group_name=group_name)
-        with self.project_hdf5.open("input") as h5in:
-            self.input.to_hdf(h5in)
-            self.structure.to_hdf(h5in)
-        if self._data is not None:
-            with self.project_hdf5.open("output") as hdf5_out:
-                hdf5_out["average"] = self._data["average"]
-                hdf5_out["fe_total"] = self._data['results']['free_energy']
-                hdf5_out["fe_error"] = self._data['results']['error']
-                hdf5_out["fe_reference"] = self._data['results']['reference_system']
-                hdf5_out["fe_work"] = self._data['results']['work']
-                hdf5_out["fe_pv"] = self._data['results']['pv']
-                hdf5_out["temperature"] = self.input.temperature
-
-                if self.input.mode == "ts":
-                    datfile = os.path.join(self.working_directory, "temperature_sweep.dat")
-                    t, fe, ferr = np.loadtxt(datfile, unpack=True, usecols=(0,1,2))
-                    hdf5_out["fe_total"] = fe
-                    hdf5_out["temperature"] = t
-                    hdf5_out["fe_error"] = ferr
-
-    def from_hdf(self, hdf=None, group_name=None):
-        super().from_hdf(hdf=hdf, group_name=group_name)
-        with self.project_hdf5.open("input") as h5in:
-            self.input.from_hdf(h5in)
-            self.structure.from_hdf(h5in)
-
-    def collect_logs(self):
-        logile = os.path.join(self.working_directory, "calphy.log")
-        with open(logile, 'r') as fin:
-            content = fin.read()
-        with self.project_hdf5.open("output") as hdf5_out:
-            hdf5_out["calphy_log"] = content
-        logile = os.path.join(self.working_directory, "log.lammps")
-        with open(logile, 'r') as fin:
-            content = fin.read()
-        with self.project_hdf5.open("output") as hdf5_out:
-            hdf5_out["lammps_log"] = content
-
-    def collect_files(self):
-        datfile = os.path.join(self.working_directory, "temperature_sweep.dat")
-        t, fe, ferr = np.loadtxt(datfile, unpack=True, usecols=(0,1,2))
-
-
-class Input:
+class Input(DataContainer):
     def __init__(self):
-        self.element = None
-        self.mass = 1.00
+        super(Input, self).__init__(table_name="input")
         self.md = {
             #pair elements
             "pair_style": None, "pair_coeff": None,
@@ -161,88 +35,263 @@ class Input:
             "walltime": "23:50:00", "queuename": None, "memory": "3GB",
             "commands": None, "modules": None, "options": None
         }
-        self.temperature = None
-        self.pressure = None
-        self.potential = LammpsPotential()
-        self.state = None
-        self.nsims = 1
-        self._mode = "fe"
-        self.iso = False
+        self.mode = None
+        self._potential = None
+        self.tguess = None
+        self.structure = None
+        self.options = {}
+        
+        #Temperature
+        self._temperature = [0]
+
+        #Pressure
+        self._pressure = 0
+        self.iso = True
         self.fix_lattice = False
         self.npt = True
-        self.tguess = None
-        self.options = {}
+
+        self.reference_state = None
+        self.n_cycles = 1
+        self.n_switching_steps = 25000
+        self.n_equilibration_steps = 10000
 
     @property
-    def mode(self):
-        return self._mode
-    
-    @mode.setter
-    def mode(self, value):
-        if value in ["fe", "ts"]:
-            self._mode = value
+    def temperature(self):
+        return self._temperature
+
+    @temperature.setter
+    def temperature(self, value):
+        """
+        float/list of 2 entries
+        """
+        if isinstance(value, list):
+            if not len(value) == 2:
+                raise ValueError("Temperature should be float of list of 2 floats")
         else:
-            raise ValueError("Currently only mode fe is supported")
+            value = [value]
+        self._temperature = value
+
+
+    @property
+    def pressure(self):
+        return self._pressure
+
+    @pressure.setter
+    def pressure(self, value):
+        """
+        None: fix_lattice True, else fix_lattice False
+        scalar: iso True
+        vector: iso False
+        npt: for the moment, True
+        """
+        if value is None:
+            self.fix_lattice = True
+        elif isinstance(value, list):
+            if len(value) == 3:
+                if (value[0]==value[1]==value[2]):
+                    self._pressure = value[0]
+                    self.iso = False
+                else:
+                    raise NotImplementedError()
+            else:
+                raise NotImplementedError()
+        else:
+            self._pressure = value
+            self.iso = True
+            self.fix_lattice = False
+                
     
-    def structure_to_lammps(self):
-        prism = UnfoldingPrism(self.structure.cell)
-        lammps_structure = self.structure.copy()
+    @property
+    def potential(self):
+        return self._potential.df
+    
+    @potential.setter
+    def potential(self, potential_filename):
+        potential_db = LammpsPotentialFile()
+        potential = potential_db.find_by_name(potential_filename)
+        self._potential = LammpsPotential()
+        self._potential.df = potential
+    
+    def structure_to_lammps(self, structure):
+        prism = UnfoldingPrism(structure.cell)
+        lammps_structure = structure.copy()
         lammps_structure.set_cell(prism.A)
-        lammps_structure.positions = np.matmul(self.structure.positions, prism.R)
+        lammps_structure.positions = np.matmul(structure.positions, prism.R)
         return lammps_structure
     
-    def write_structure(self, file_name, working_directory):
+    def write_structure(self, structure, file_name, working_directory):
         lmp_structure = LammpsStructure()
         lmp_structure.potential = self.potential
-        lmp_structure.el_eam_lst = set(self.structure.get_chemical_symbols())
-        lmp_structure.structure = self.structure_to_lammps()
+        lmp_structure.el_eam_lst = set(structure.get_chemical_symbols())
+        lmp_structure.structure = self.structure_to_lammps(structure)
         lmp_structure.write_file(file_name=file_name, cwd=working_directory)
+    
+    def determine_mode(self):
+        if len(self.temperature) == 1:
+            self.mode = "fe"
+        elif len(self.temperature) == 2:
+            self.mode = "ts"
+        #if mode was not set, raise Error
+        if self.mode is None:
+            raise RuntimeError("Could not determine the mode")
+            
+    def prepare_input(self, structure, working_directory, cores=1):
+                
+        #self.structure = structure
+        file_name = "conf.data"
+        self.write_structure(structure, file_name, working_directory)      
+        self._potential.copy_pot_files(working_directory)
         
-    def prepare_input(self, structure, cores=1):
-        self.structure = structure
-        self.options["element"] = check_and_convert_to_list(self.element)
+        #this still needs to be fixed
+        self.options["element"] = np.unique(structure.get_chemical_symbols())
         self.options["nelements"] = len(self.options["element"])
-        self.options["mass"] = check_and_convert_to_list(self.mass)
+        self.options["mass"] = np.unique(structure.get_masses())
+        
         self.options["md"] = self.md
+        self.options["md"]["te"] = self.n_equilibration_steps
+        self.options["md"]["ts"] = self.n_switching_steps
+        
         self.options["conv"] = self.conv
         self.options["queue"] = self.queue
+        
         #add parallel support if needed
         self.options["queue"]["cores"] = cores
-        self.options["md"]["pair_style"] = self.potential.df['Config'].to_list()[0][0].strip().split()[1:]
-        self.options["md"]["pair_coeff"] = [" ".join(self.potential.df['Config'].to_list()[0][1].strip().split()[1:])]
+        
+        #prepare potentials
+        pair_style = []
+        pair_coeff = []
+        pair_style.append(self._potential.df['Config'].to_list()[0][0].strip().split()[1:][0])
+        pair_coeff.append(" ".join(self._potential.df['Config'].to_list()[0][1].strip().split()[1:]))
+        
+        self.options["md"]["pair_style"] = pair_style
+        self.options["md"]["pair_coeff"] = pair_coeff
         self.options["calculations"] = []
+        
         #this is also to satisfy calphys algorithm; it can take a number of calculations
         #at the same time. But this is likely what we do not want here.
         cdict = {}
         cdict["mode"] = self.mode
-        if isinstance(self.temperature, list):
-            cdict["temperature"] = self.temperature[0]
-            cdict["temperature_stop"] = self.temperature[-1]
+        
+        if isinstance(self._temperature, list):
+            cdict["temperature"] = self._temperature[0]
+            cdict["temperature_stop"] = self._temperature[-1]
         else:
-            cdict["temperature"] = self.temperature
-            cdict["temperature_stop"] = self.temperature
-        cdict["lattice"] = None
-        cdict["pressure"] = self.pressure
-        cdict["state"] = self.state
-        cdict["nelements"] = len(self.options["element"])
+            cdict["temperature"] = self._temperature
+            cdict["temperature_stop"] = self._temperature
+        
+        cdict["lattice"] = os.path.join(working_directory, "conf.data")
+        cdict["state"] = self.reference_state
+        cdict["nelements"] = self.options["nelements"]
         cdict["element"] = self.options["element"]
         cdict["lattice_constant"] = 0
+
+        cdict["pressure"] = self._pressure
         cdict["iso"] = self.iso
         cdict["fix_lattice"] = self.fix_lattice
         cdict["npt"] = self.npt
+        
         cdict["tguess"] = self.tguess
         cdict["repeat"] = [1, 1, 1]
-        cdict["nsims"] = self.nsims
+        cdict["nsims"] = self.n_cycles
         cdict["thigh"] = 2.0*cdict["temperature_stop"]
         cdict["dtemp"] = 200
         cdict["maxattempts"] = 5
         self.options["calculations"].append(cdict)
     
-    def to_hdf(self, hdf5):
-        self.potential.to_hdf(hdf5)
-        hdf5["input"] = self.options
+class CalphyBase(GenericJob):
+    def __init__(self, project, job_name):
+        super(CalphyBase, self).__init__(project, job_name)
+        self.__name__ = "CalphyJob"
+        self.structure = False
+        self.input = Input()
+        #self.output = DataContainer(table_name="output")
+        
+        self._data = None
     
-    def from_hdf(self, hdf5):
-        self.potential.from_hdf(hdf5)
-        self.options = hdf5["input"]
+    #we wrap some properties for easy access
+    @property
+    def potential(self):
+        return self.input.potential
+    
+    @potential.setter
+    def potential(self, potential_filename):
+        self.input.potential = potential_filename
+    
+    def view_potentials(self):
+        if not self.structure:
+            list_of_elements = set(self.input.element)
+        else:    
+            list_of_elements = set(self.structure.get_chemical_symbols())
+        list_of_potentials = LammpsPotentialFile().find(list_of_elements)
+        if list_of_potentials is not None:
+            return list_of_potentials
+        else:
+            raise TypeError(
+                "No potentials found for this kind of structure: ",
+                str(list_of_elements),)
+    
+    def write_input(self):
+        self.input.prepare_input(self.structure, self.working_directory, 
+                                 cores = self.server.cores)
+    
+    def calc_mode_fe(self):
+        self.input.mode = "fe"
 
+    def calc_mode_ts(self):
+        self.input.mode = "ts"
+
+    def calc_free_energy(self):
+        self.input.determine_mode()
+    
+    def run_static(self):
+        self.status.running = True
+        if self.input.reference_state == "solid":
+            job = Solid(options=self.input.options, 
+                         kernel=0, 
+                         simfolder=self.working_directory)
+        elif self.input.reference_state == "liquid":
+            job = Liquid(options=self.input.options, 
+                         kernel=0, 
+                         simfolder=self.working_directory)
+        else:
+            raise ValueError("Unknown reference state")
+        
+        if self.input.mode == "fe":
+            routine_fe(job)        
+        elif self.input.mode == "ts":
+            routine_ts(job)
+        else:
+            raise ValueError("Unknown mode")
+        self._data = job.report
+        del self._data['input']
+        self.status.collect = True
+        self.run()
+    
+
+    def collect_general_output(self):
+        #self.output.spring_constant = self._data["average"]["spring_constant"]
+        if self._data is not None:
+            with self.project_hdf5.open("output") as hdf5_out:
+                #hdf5_out["spring_constant"] = self._data["average"]["spring_constant"]
+                hdf5_out["energy_free"] = self._data['results']['free_energy']
+                hdf5_out["energy_free_error"] = self._data['results']['error']
+                hdf5_out["temperature"] = self.input.temperature
+
+                if self.input.mode == "ts":
+                        datfile = os.path.join(self.working_directory, "temperature_sweep.dat")
+                        t, fe, ferr = np.loadtxt(datfile, unpack=True, usecols=(0,1,2))                
+                        hdf5_out["energy_free"] = fe
+                        hdf5_out["energy_free_error"] = ferr
+                        hdf5_out["temperature"] = t
+
+    def collect_output(self):
+        self.to_hdf()
+    
+    def to_hdf(self, hdf=None, group_name=None):
+        super().to_hdf(hdf=hdf, group_name=group_name)
+        self.collect_general_output()
+        #self.structure.to_hdf(hdf)
+        #with self.project_hdf5.open("input") as hdf5_in:
+        #self.input.to_hdf(self.project_hdf5)
+        #with self.project_hdf5.open("output") as hdf5_out:
+        #self.output.to_hdf(self.project_hdf5)
