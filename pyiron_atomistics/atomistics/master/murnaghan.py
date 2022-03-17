14c2c38d92dc9df55e702f791125e3e54b7188f1
Marvin Poul <ponder@creshal.de>
2022-03-17 16:18:36 +0100

    Allow strain along individual axes

--- pyiron_atomistics/atomistics/master/murnaghan.py
+++ pyiron_atomistics/atomistics/master/murnaghan.py
@@ -323,13 +323,15 @@ class MurnaghanJobGenerator(JobGenerator):
             (list)
         """
         parameter_lst = []
+        axes = np.array([a in self._master.input["axes"] for a in ('x', 'y', 'z')])
+        num_axes = sum(axes)
         for strain in np.linspace(
-            1 - self._master.input["vol_range"],
-            1 + self._master.input["vol_range"],
+            -self._master.input["vol_range"],
+            +self._master.input["vol_range"],
             int(self._master.input["num_points"]),
         ):
-            basis = self._master.ref_job.structure.copy()
-            basis.set_cell(basis.cell * strain ** (1.0 / 3.0), scale_atoms=True)
+            strains = axes * strain**(1.0/num_axes)
+            basis = self._master.ref_job.structure.apply_strain(strains, return_box=True)
             parameter_lst.append([np.round(strain, 7), basis])
         return parameter_lst
 
@@ -651,6 +653,7 @@ class Murnaghan(AtomisticParallelMaster):
             0.1,
             "relative volume variation around volume defined by ref_ham",
         )
+        self.input["axes"] = (["x", "y", "z"], "Axes along which the strain will be applied")
 
         self.debye_model = DebyeModel(self)
         self.fit_module = EnergyVolumeFit()
