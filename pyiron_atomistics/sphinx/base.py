188012bd652851f3de7c1c39658198c824e214a0
skatnagallu <s.katnagallu@mpie.de>
2022-03-31 12:29:07 +0200

    removed a possibly wrong if statement

--- pyiron_atomistics/sphinx/base.py
+++ pyiron_atomistics/sphinx/base.py
@@ -460,8 +460,8 @@ class SphinxBase(GenericDFTJob):
             self.input.sphinx.main[optimizer] = Group(table_name="input")
             self.input.sphinx.main[optimizer]["maxSteps"] = str(self.input["Istep"])
             self.input.sphinx.main[optimizer]["maxStepLength"] = str(0.1/BOHR_TO_ANGSTROM)
-            if "dE" in self.input and "dF" in self.input:
-                self.input["dE"] = 1e-3
+            # if "dE" in self.input and "dF" in self.input:
+                # self.input["dE"] = 1e-3
             if "dE" in self.input:
                 self.input.sphinx.main[optimizer]["dEnergy"] = str(
                     self.input["dE"] / HARTREE_TO_EV
