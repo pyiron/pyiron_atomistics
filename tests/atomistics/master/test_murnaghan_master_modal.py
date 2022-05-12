19ce1675c8929461e12856cc120eb3ab5070b6a6
Marvin Poul <poul@mpie.de>
2022-05-12 10:54:16 +0000

    Fix type in Murnaghan test

--- tests/atomistics/master/test_murnaghan_master_modal.py
+++ tests/atomistics/master/test_murnaghan_master_modal.py
@@ -62,7 +62,7 @@ class TestMurnaghan(TestWithCleanProject):
         murn.run()
         self.assertFalse(ham.status.finished)
         self.project.wait_for_job(murn, interval_in_s=5, max_iterations=50)
-        self.assertTrue(murn.status.not_converged or murn.status.finised)
+        self.assertTrue(murn.status.not_converged or murn.status.finished)
 
 
 if __name__ == "__main__":
