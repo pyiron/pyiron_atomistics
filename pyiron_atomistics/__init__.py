06717c69cbdaa3f75ff159c076f907530fed7672
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2022-07-13 11:33:16 +0200

    Use JobType.register to add new job types

--- pyiron_atomistics/__init__.py
+++ pyiron_atomistics/__init__.py
@@ -8,7 +8,7 @@ from pyiron_atomistics.atomistics.structure.atoms import (
     pyiron_to_ase,
     Atoms,
 )
-from pyiron_base import Notebook, install_dialog, JOB_CLASS_DICT
+from pyiron_base import Notebook, install_dialog, JobType
 
 from pyiron_base import Project as ProjectBase
 
@@ -24,51 +24,51 @@ create_ase_bulk = _StructureFactory().ase.bulk
 create_structure = _StructureFactory.crystal
 
 # Make classes available for new pyiron version
-JOB_CLASS_DICT["ART"] = "pyiron_atomistics.interactive.activation_relaxation_technique"
-JOB_CLASS_DICT["AtomisticExampleJob"] = "pyiron_atomistics.testing.randomatomistic"
-JOB_CLASS_DICT["Calphy"] = "pyiron_atomistics.calphy.job"
-JOB_CLASS_DICT[
-    "ConvEncutParallel"
-] = "pyiron_atomistics.dft.master.convergence_encut_parallel"
-JOB_CLASS_DICT[
-    "ConvEncutSerial"
-] = "pyiron_atomistics.dft.master.convergence_encut_serial"
-JOB_CLASS_DICT[
-    "ConvergenceVolume"
-] = "pyiron_atomistics.atomistics.master.convergence_volume"
-JOB_CLASS_DICT[
-    "ConvKpointParallel"
-] = "pyiron_atomistics.dft.master.convergence_kpoint_parallel"
-JOB_CLASS_DICT["ElasticTensor"] = "pyiron_atomistics.atomistics.master.elastic"
-JOB_CLASS_DICT["ExampleJob"] = "pyiron_atomistics.testing.randomatomistic"
-JOB_CLASS_DICT["Gaussian"] = "pyiron_atomistics.gaussian.gaussian"
-JOB_CLASS_DICT["Gpaw"] = "pyiron_atomistics.gpaw.gpaw"
-JOB_CLASS_DICT["HessianJob"] = "pyiron_atomistics.thermodynamics.hessian"
-JOB_CLASS_DICT["Lammps"] = "pyiron_atomistics.lammps.lammps"
-JOB_CLASS_DICT["MapMaster"] = "pyiron_atomistics.atomistics.master.parallel"
-JOB_CLASS_DICT["Murnaghan"] = "pyiron_atomistics.atomistics.master.murnaghan"
-JOB_CLASS_DICT["MurnaghanDFT"] = "pyiron_atomistics.dft.master.murnaghan_dft"
-JOB_CLASS_DICT["PhonopyJob"] = "pyiron_atomistics.atomistics.master.phonopy"
-JOB_CLASS_DICT["QuasiHarmonicJob"] = "pyiron_atomistics.atomistics.master.quasi"
-JOB_CLASS_DICT["QuasiNewton"] = "pyiron_atomistics.interactive.quasi_newton"
-JOB_CLASS_DICT["ScipyMinimizer"] = "pyiron_atomistics.interactive.scipy_minimizer"
-JOB_CLASS_DICT["SerialMaster"] = "pyiron_atomistics.atomistics.master.serial"
-JOB_CLASS_DICT["Sphinx"] = "pyiron_atomistics.sphinx.sphinx"
-JOB_CLASS_DICT[
-    "StructureContainer"
-] = "pyiron_atomistics.atomistics.job.structurecontainer"
-JOB_CLASS_DICT["StructureListMaster"] = "pyiron_atomistics.atomistics.master.structure"
-JOB_CLASS_DICT["SQSJob"] = "pyiron_atomistics.atomistics.job.sqs"
-JOB_CLASS_DICT["SQSMaster"] = "pyiron_atomistics.atomistics.master.sqsmaster"
-JOB_CLASS_DICT["SxDynMat"] = "pyiron_atomistics.thermodynamics.sxphonons"
-JOB_CLASS_DICT["SxExtOptInteractive"] = "pyiron_atomistics.interactive.sxextoptint"
-JOB_CLASS_DICT["SxHarmPotTst"] = "pyiron_atomistics.thermodynamics.sxphonons"
-JOB_CLASS_DICT["SxPhonons"] = "pyiron_atomistics.thermodynamics.sxphonons"
-JOB_CLASS_DICT["SxUniqDispl"] = "pyiron_atomistics.thermodynamics.sxphonons"
-JOB_CLASS_DICT["TableJob"] = "pyiron_atomistics.table.datamining"
-JOB_CLASS_DICT["Vasp"] = "pyiron_atomistics.vasp.vasp"
-JOB_CLASS_DICT["VaspMetadyn"] = "pyiron_atomistics.vasp.metadyn"
-JOB_CLASS_DICT["VaspSol"] = "pyiron_atomistics.vasp.vaspsol"
+JobType.register("pyiron_atomistics.interactive.activation_relaxation_technique", "ART")
+JobType.register("pyiron_atomistics.testing.randomatomistic", "AtomisticExampleJob")
+JobType.register("pyiron_atomistics.calphy.job", "Calphy")
+JobType.register(
+    "pyiron_atomistics.dft.master.convergence_encut_parallel", "ConvEncutParallel"
+)
+JobType.register(
+    "pyiron_atomistics.dft.master.convergence_encut_serial", "ConvEncutSerial"
+)
+JobType.register(
+    "pyiron_atomistics.atomistics.master.convergence_volume", "ConvergenceVolume"
+)
+JobType.register(
+    "pyiron_atomistics.dft.master.convergence_kpoint_parallel", "ConvKpointParallel"
+)
+JobType.register("pyiron_atomistics.atomistics.master.elastic", "ElasticTensor")
+JobType.register("pyiron_atomistics.testing.randomatomistic", "ExampleJob")
+JobType.register("pyiron_atomistics.gaussian.gaussian", "Gaussian")
+JobType.register("pyiron_atomistics.gpaw.gpaw", "Gpaw")
+JobType.register("pyiron_atomistics.thermodynamics.hessian", "HessianJob")
+JobType.register("pyiron_atomistics.lammps.lammps", "Lammps")
+JobType.register("pyiron_atomistics.atomistics.master.parallel", "MapMaster")
+JobType.register("pyiron_atomistics.atomistics.master.murnaghan", "Murnaghan")
+JobType.register("pyiron_atomistics.dft.master.murnaghan_dft", "MurnaghanDFT")
+JobType.register("pyiron_atomistics.atomistics.master.phonopy", "PhonopyJob")
+JobType.register("pyiron_atomistics.atomistics.master.quasi", "QuasiHarmonicJob")
+JobType.register("pyiron_atomistics.interactive.quasi_newton", "QuasiNewton")
+JobType.register("pyiron_atomistics.interactive.scipy_minimizer", "ScipyMinimizer")
+JobType.register("pyiron_atomistics.atomistics.master.serial", "SerialMaster")
+JobType.register("pyiron_atomistics.sphinx.sphinx", "Sphinx")
+JobType.register(
+    "pyiron_atomistics.atomistics.job.structurecontainer", "StructureContainer"
+)
+JobType.register("pyiron_atomistics.atomistics.master.structure", "StructureListMaster")
+JobType.register("pyiron_atomistics.atomistics.job.sqs", "SQSJob")
+JobType.register("pyiron_atomistics.atomistics.master.sqsmaster", "SQSMaster")
+JobType.register("pyiron_atomistics.thermodynamics.sxphonons", "SxDynMat")
+JobType.register("pyiron_atomistics.interactive.sxextoptint", "SxExtOptInteractive")
+JobType.register("pyiron_atomistics.thermodynamics.sxphonons", "SxHarmPotTst")
+JobType.register("pyiron_atomistics.thermodynamics.sxphonons", "SxPhonons")
+JobType.register("pyiron_atomistics.thermodynamics.sxphonons", "SxUniqDispl")
+JobType.register("pyiron_atomistics.table.datamining", "TableJob")
+JobType.register("pyiron_atomistics.vasp.vasp", "Vasp")
+JobType.register("pyiron_atomistics.vasp.metadyn", "VaspMetadyn")
+JobType.register("pyiron_atomistics.vasp.vaspsol", "VaspSol")
 
 from ._version import get_versions
 
