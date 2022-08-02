b258670348dea75b012b4533d246b3f3fcc79d86
Marvin Poul <poul@mpie.de>
2022-08-02 16:18:02 +0200

    Add plotting interface to StructureStorage
    This is code moved from pyiron_contrib

--- pyiron_atomistics/atomistics/structure/structurestorage.py
+++ pyiron_atomistics/atomistics/structure/structurestorage.py
@@ -8,7 +8,9 @@ Alternative structure container that stores them in flattened arrays.
 
 from typing import List
 
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 from pyiron_base import FlattenedStorage
 from pyiron_atomistics.atomistics.structure.atom import Atom
@@ -241,3 +243,94 @@ class StructureStorage(FlattenedStorage, HasStructure):
 
     def _get_hdf_group_name(self):
         return "structures"
+
+    @property
+    def plot(self):
+        """
+        Accessor for :class:`.StructurePlots` instance using these structures.
+        """
+        return StructurePlots(self)
+
+
+class StructurePlots:
+    """
+    Simple interface to plot various properties of structures.
+    """
+
+    def __init__(self, store: StructureStorage):
+        self._store = store
+
+    def cell(self, angle_in_degrees=True):
+        """
+        Plot histograms of cell parameters.
+
+        Plotted are atomic volume, density, cell vector lengths and cell vector angles in separate subplots all on a
+        log-scale.
+
+        Args:
+            angle_in_degrees (bool): whether unit for angles is degree or radians
+
+        Returns:
+            `DataFrame`: contains the plotted information in the columns:
+                            - a: length of first vector
+                            - b: length of second vector
+                            - c: length of third vector
+                            - alpha: angle between first and second vector
+                            - beta: angle between second and third vector
+                            - gamma: angle between third and first vector
+                            - V: volume of the cell
+                            - N: number of atoms in the cell
+        """
+        N = self._store.get_array("length")
+        C = self._store.get_array("cell")
+
+        def get_angle(cell, idx=0):
+            return np.arccos(
+                np.dot(cell[idx], cell[(idx + 1) % 3])
+                / np.linalg.norm(cell[idx])
+                / np.linalg.norm(cell[(idx + 1) % 3])
+            )
+
+        def extract(n, c):
+            return {
+                "a": np.linalg.norm(c[0]),
+                "b": np.linalg.norm(c[1]),
+                "c": np.linalg.norm(c[2]),
+                "alpha": get_angle(c, 0),
+                "beta": get_angle(c, 1),
+                "gamma": get_angle(c, 2),
+            }
+
+        df = pd.DataFrame([extract(n, c) for n, c in zip(N, C)])
+        df["V"] = np.linalg.det(C)
+        df["N"] = N
+        if angle_in_degrees:
+            df["alpha"] = np.rad2deg(df["alpha"])
+            df["beta"] = np.rad2deg(df["beta"])
+            df["gamma"] = np.rad2deg(df["gamma"])
+
+        plt.subplot(1, 4, 1)
+        plt.title("Atomic Volume")
+        plt.hist(df.V / df.N, bins=20, log=True)
+        plt.xlabel(r"$V$ [$\AA^3$]")
+
+        plt.subplot(1, 4, 2)
+        plt.title("Density")
+        plt.hist(df.N / df.V, bins=20, log=True)
+        plt.xlabel(r"$\rho$ [$\AA^{-3}$]")
+
+        plt.subplot(1, 4, 3)
+        plt.title("Lattice Vector Lengths")
+        plt.hist([df.a, df.b, df.c], log=True)
+        plt.xlabel(r"$a,b,c$ [$\AA$]")
+
+        plt.subplot(1, 4, 4)
+        plt.title("Lattice Vector Angles")
+        plt.hist([df.alpha, df.beta, df.gamma], log=True)
+        if angle_in_degrees:
+            label = r"$\alpha,\beta,\gamma$ [°]"
+        else:
+            label = r"$\alpha,\beta,\gamma$ [rad]"
+        plt.xlabel(label)
+
+        return df
