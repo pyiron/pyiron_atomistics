24aaedbb1bdf60868fec114970acdb07d9cac587
samwaseda <o.waseda@mpie.de>
2023-04-16 18:25:23 +0000

    make it possible to choose potentials

--- pyiron_atomistics/lammps/potentials.py
+++ pyiron_atomistics/lammps/potentials.py
@@ -16,9 +16,15 @@ __date__ = "Sep 1, 2017"
 import pandas as pd
 from pyiron_atomistics.lammps.potential import LammpsPotentialFile
 import numpy as np
+import warnings
 
 
 class LammpsPotential:
+    def __new__(cls, *args, **kwargs):
+        obj = super().__new__(cls)
+        cls._df = None
+        return obj
+
     def copy(self):
         new_pot = LammpsPotential()
         new_pot.set_df(self.get_df())
@@ -34,16 +40,16 @@ class LammpsPotential:
 
     @property
     def model(self):
-        return "_and_".join(set(self._df.model))
+        return "_and_".join(set(self.df.model))
 
     @property
     def name(self):
-        return "_and_".join(set(self._df.name))
+        return "_and_".join(set(self.df.name))
 
     @property
     def species(self):
-        species = set([ss for s in self._df.interacting_species for ss in s])
-        preset = set(["___".join(s) for s in self._df.preset_species if len(s) > 0])
+        species = set([ss for s in self.df.interacting_species for ss in s])
+        preset = set(["___".join(s) for s in self.df.preset_species if len(s) > 0])
         if len(preset) == 0:
             return list(species)
         elif len(preset) > 1:
@@ -53,25 +59,25 @@ class LammpsPotential:
 
     @property
     def filename(self):
-        return [f for f in set(self._df.filename) if len(f) > 0]
+        return [f for f in set(self.df.filename) if len(f) > 0]
 
     @property
     def citations(self):
-        return "".join(np.unique([c for c in self._df.citations if len(c) > 0]))
+        return "".join(np.unique([c for c in self.df.citations if len(c) > 0]))
 
     @property
     def is_scaled(self):
-        return "scale" in self._df
+        return "scale" in self.df
 
     @property
     def pair_style(self):
-        if len(set(self._df.pair_style)) == 1:
-            return "pair_style " + list(set(self._df.pair_style))[0]
-        elif "scale" not in self._df:
+        if len(set(self.df.pair_style)) == 1:
+            return "pair_style " + list(set(self.df.pair_style))[0]
+        elif "scale" not in self.df:
             return "pair_style hybrid"
-        elif all(self._df.scale == 1):
-            return "pair_style hybrid/overlay " + " ".join(list(self._df.pair_style))
-        return "pair_style hybrid/scaled " + " ".join([str(ss) for s in self._df[["scale", "pair_style"]].values for ss in s])
+        elif all(self.df.scale == 1):
+            return "pair_style hybrid/overlay " + " ".join(list(self.df.pair_style))
+        return "pair_style hybrid/scaled " + " ".join([str(ss) for s in self.df[["scale", "pair_style"]].values for ss in s])
 
     @property
     def pair_coeff(self):
@@ -86,20 +92,20 @@ class LammpsPotential:
                 " ".join(
                     ["pair_coeff"] + convert(c[0]) + [c[1]] + [c[2]] + ["\n"]
                 )
-                for c in self._df[["interacting_species", "pair_style", "pair_coeff"]].values
+                for c in self.df[["interacting_species", "pair_style", "pair_coeff"]].values
             ]
         return [
             " ".join(
                 ["pair_coeff"] + convert(c[0]) + [c[1]] + ["\n"]
             )
-            for c in self._df[["interacting_species", "pair_coeff"]].values
+            for c in self.df[["interacting_species", "pair_coeff"]].values
         ]
 
     def __repr__(self):
-        return self._df.__repr__()
+        return self.df.__repr__()
 
     def _repr_html_(self):
-        return self._df._repr_html_()
+        return self.df._repr_html_()
 
     def set_df(self, df):
         for key in ["pair_style", "interacting_species", "pair_coeff", "preset_species"]:
@@ -107,10 +113,14 @@ class LammpsPotential:
                 raise ValueError(f"{key} missing")
         self._df = df
 
+    @property
+    def df(self):
+        return self._df
+
     def get_df(self, default_scale=None):
-        if default_scale is None or "scale" in self._df:
-            return self._df.copy()
-        df = self._df.copy()
+        if default_scale is None or "scale" in self.df:
+            return self.df.copy()
+        df = self.df.copy()
         df["scale"] = 1
         return df
 
@@ -124,7 +134,7 @@ class LammpsPotential:
         if self.is_scaled:
             raise NotImplementedError("Currently you cannot scale twice")
         new_pot = self.copy()
-        new_pot._df['scale'] = scale_or_potential
+        new_pot.df['scale'] = scale_or_potential
         return new_pot
 
     __rmul__ = __mul__
@@ -207,22 +217,35 @@ class EAM(LammpsPotential):
         return
 
     def __init__(self, *chemical_elements, name=None, pair_style=None):
-        if name is None:
-            df = LammpsPotentialFile().find(list(chemical_elements)).iloc[0]
+        if name is not None:
+            self._df_candidates = LammpsPotentialFile().find_by_name(name)
         else:
-            df = LammpsPotentialFile().find_by_name(name).iloc[0]
-        pair_style = df.Config[0].split()[1].replace('\n', '')
-        self._initialize_df(
-            pair_style=self._get_pair_style(df.Config),
-            interacting_species=self._get_interacting_species(df.Config, df.Species),
-            pair_coeff=self._get_pair_coeff(df.Config),
-            preset_species=[df.Species],
-            model=df.Model,
-            citations=df.Citations,
-            filename=df.Filename,
-            name=df.Name,
-            scale=self._get_scale(df.Config)
-        )
+            self._df_candidates = LammpsPotentialFile().find(list(chemical_elements))
+
+    def list_potentials(self):
+        return self._df_candidates.Name
+
+    def view_potentials(self):
+        return self._df_candidates
+
+    @property
+    def df(self):
+        if self._df is None:
+            df = self._df_candidates.iloc[0]
+            if len(self._df_candidates) > 1:
+                warnings.warn(f"Potential not specified - chose {df.Name}")
+            self._initialize_df(
+                pair_style=self._get_pair_style(df.Config),
+                interacting_species=self._get_interacting_species(df.Config, df.Species),
+                pair_coeff=self._get_pair_coeff(df.Config),
+                preset_species=[df.Species],
+                model=df.Model,
+                citations=df.Citations,
+                filename=df.Filename,
+                name=df.Name,
+                scale=self._get_scale(df.Config)
+            )
+        return self._df
 
 
 class Morse(LammpsPotential):
