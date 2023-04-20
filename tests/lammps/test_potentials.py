11f2304555decd9b1bed7fa831b32a1e52bf668b
samwaseda <o.waseda@mpie.de>
2023-04-20 07:51:34 +0000

    add test_set_df

--- tests/lammps/test_potentials.py
+++ tests/lammps/test_potentials.py
@@ -4,6 +4,7 @@
 
 from pyiron_atomistics.lammps.potentials import Library, Morse, CustomPotential, LammpsPotentials
 import unittest
+import pandas as pd
 
 
 class TestPotentials(unittest.TestCase):
@@ -13,6 +14,22 @@ class TestPotentials(unittest.TestCase):
         for i in [2, 3, 4]:
             self.assertEqual(pot._harmonize_species(i * ("Al",)), i * ["Al"])
         self.assertRaises(ValueError, pot._harmonize_species, tuple())
+
+    def test_set_df(self):
+        pot = LammpsPotentials()
+        self.assertEqual(pot.df, None)
+        required_keys = [
+            "pair_style",
+            "interacting_species",
+            "pair_coeff",
+            "preset_species",
+        ]
+        arg_dict = {k: [] for k in required_keys}
+        pot.set_df(pd.DataFrame(arg_dict))
+        self.assertIsInstance(pot.df, pd.DataFrame)
+        for key in required_keys:
+            arg_dict = {k: [] for k in required_keys if k != key}
+            self.assertRaises(ValueError, pot.set_df, pd.DataFrame(arg_dict))
 
 
 if __name__ == "__main__":
