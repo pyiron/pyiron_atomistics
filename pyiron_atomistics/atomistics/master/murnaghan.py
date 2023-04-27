correct unit conversion

pyiron_atomistics/atomistics/master/murnaghan.py
@@ -10,6 +10,7 @@ import numpy as np
 import scipy.constants
 import scipy.integrate
 import scipy.optimize as spy
+from pint import UnitRegistry
 
 from pyiron_atomistics.atomistics.master.parallel import AtomisticParallelMaster
 from pyiron_atomistics.atomistics.structure.atoms import Atoms, ase_to_pyiron
@@ -55,7 +56,7 @@ def birchmurnaghan_energy(V, E0, B0, BP, V0):
     )
 
 
-e_doc_str += """
+e_doc_str = """
     Args:
         V (numpy.ndarray): Array of volumes
         E0 (float): Minimum energy value
@@ -626,17 +627,17 @@ class EnergyVolumeFit(object):
     @staticmethod
     def birchmurnaghan_energy(V, E0, B0, BP, V0):
         "BirchMurnaghan equation from PRB 70, 224107"
-        return birchmurnaghan_energy(V, E0, B0, BP, V0)
+        return birchmurnaghan_energy(V, E0, B0 / eV_div_A3_to_GPa, BP, V0)
 
     @staticmethod
     def vinet_energy(V, E0, B0, BP, V0):
         "Vinet equation from PRB 70, 224107"
-        return vinet_energy(V, E0, B0, BP, V0)
+        return vinet_energy(V, E0, B0 / eV_div_A3_to_GPa, BP, V0)
 
     @staticmethod
     def murnaghan(V, E0, B0, BP, V0):
         "From PRB 28,5480 (1983"
-        return murnaghan(V, E0, B0, BP, V0)
+        return murnaghan(V, E0, B0 / eV_div_A3_to_GPa, BP, V0)
 
     @staticmethod
     def birch(V, E0, B0, BP, V0):
@@ -647,11 +648,11 @@ class EnergyVolumeFit(object):
 
         case where n=0
         """
-        return birch(V, E0, B0, BP, V0)
+        return birch(V, E0, B0 / eV_div_A3_to_GPa, BP, V0)
 
     @staticmethod
     def pouriertarantola(V, E0, B0, BP, V0):
-        return pouriertarantola(V, E0, B0, BP, V0)
+        return pouriertarantola(V, E0, B0 / eV_div_A3_to_GPa, BP, V0)
 
 
 # ToDo: not all abstract methods implemented