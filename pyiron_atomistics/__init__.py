cedc8708d1dfa432a139c8c530a6dfd7aff87895
liamhuber <liamhuber@greyhavensolutions.com>
2023-05-10 11:23:28 -0700

    Remove non-existent imports in __init__

--- pyiron_atomistics/__init__.py
+++ pyiron_atomistics/__init__.py
@@ -3,11 +3,7 @@ __all__ = []
 
 from pyiron_atomistics.project import Project
 from pyiron_atomistics.toolkit import AtomisticsTools
-from pyiron_atomistics.atomistics.structure.atoms import (
-    ase_to_pyiron,
-    pyiron_to_ase,
-    Atoms,
-)
+from pyiron_atomistics.atomistics.structure.atoms import Atoms
 from pyiron_base import Notebook, install_dialog, JOB_CLASS_DICT
 
 from pyiron_base import Project as ProjectBase
