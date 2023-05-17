0ec517d7fe5f150617c98e2b4ae0a40315fa439d
Sam Dareska <37879103+samwaseda@users.noreply.github.com>
2023-05-17 10:18:56 +0300

    Update pyiron_atomistics/atomistics/structure/atoms.py
    Co-authored-by: Marvin Poul <poul@mpie.de>

--- pyiron_atomistics/atomistics/structure/atoms.py
+++ pyiron_atomistics/atomistics/structure/atoms.py
@@ -2076,7 +2076,7 @@ class Atoms(ASEAtoms):
 
     def __getattr__(self, item):
         try:
-            return self.arrays[item]
+            return self.arrays[item].tolist()
         except KeyError:
             return object.__getattribute__(self, item)
 
