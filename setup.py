f877a92af91a3c3fdd7b1fef734e3a1c4f6ef3c6
Marvin Poul <poul@mpie.de>
2023-08-02 17:05:15 +0200

    Add pyxtal dep back in
    since it is an optional dep for structuretoolkit

--- setup.py
+++ setup.py
@@ -58,7 +58,8 @@ setup(
         'seekpath==2.1.0',
         'scikit-learn==1.3.0',
         'spglib==2.0.2',
-        'structuretoolkit==0.0.6'
+        'structuretoolkit==0.0.6',
+        'pyxtal==0.5.9'
     ],
     cmdclass=versioneer.get_cmdclass(),
 
