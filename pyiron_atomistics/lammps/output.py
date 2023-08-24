55b311100c5233a5649a0734b01408b51f27301a
liamhuber <liamhuber@greyhavensolutions.com>
2023-08-24 12:47:21 -0700

    Shorten unit conversion

--- pyiron_atomistics/lammps/output.py
+++ pyiron_atomistics/lammps/output.py
@@ -463,7 +463,8 @@ def parse_lammps_output(
     potential_elements,
     units,
 ) -> Dict:
-    uc = UnitConverter(units)
+    convert_units = UnitConverter(units).convert_array_to_pyiron_units
+
     hdf_output = {"generic": {}, "lammps": {}}
     hdf_generic = hdf_output["generic"]
     hdf_lammps = hdf_output["lammps"]
@@ -478,15 +479,15 @@ def parse_lammps_output(
 
     if "computes" in dump_dict.keys():
         for k, v in dump_dict.pop("computes").items():
-            hdf_generic[k] = uc.convert_array_to_pyiron_units(np.array(v), label=k)
+            hdf_generic[k] = convert_units(np.array(v), label=k)
 
-    hdf_generic["steps"] = uc.convert_array_to_pyiron_units(
+    hdf_generic["steps"] = convert_units(
         np.array(dump_dict.pop("steps"), dtype=int), label="steps"
     )
 
     for k, v in dump_dict.items():
         if len(v) > 0:
-            hdf_generic[k] = uc.convert_array_to_pyiron_units(np.array(v), label=k)
+            hdf_generic[k] = convert_units(np.array(v), label=k)
 
     generic_keys_lst, pressure_dict, df = _parse_log_file_if_it_exists(
         log_lammps_full_file_name,
@@ -495,7 +496,7 @@ def parse_lammps_output(
 
     if df is not None and pressure_dict is not None and generic_keys_lst is not None:
         for k, v in df.items():
-            v = uc.convert_array_to_pyiron_units(np.array(v), label=k)
+            v = convert_units(np.array(v), label=k)
             if k in generic_keys_lst:
                 hdf_generic[k] = v
             else:  # This is a hack for backward comparability
@@ -503,7 +504,7 @@ def parse_lammps_output(
 
         # Store pressures as numpy arrays
         for key, val in pressure_dict.items():
-            hdf_generic[key] = uc.convert_array_to_pyiron_units(val, label=key)
+            hdf_generic[key] = convert_units(val, label=key)
     else:
         warnings.warn("LAMMPS warning: No log.lammps output file found.")
 
