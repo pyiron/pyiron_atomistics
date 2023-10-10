058c02f14c69522adb9ac58a1dc08eaf136e521f
liamhuber <liamhuber@greyhavensolutions.com>
2023-10-10 11:48:31 -0700

    Add dependency

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         'phonopy==2.20.0',
         'pint==0.22',
         'pyiron_base==0.6.7',
+        'pyiron-data==0.0.22',
         'scipy==1.11.3',
         'seekpath==2.1.0',
         'scikit-learn==1.3.1',
