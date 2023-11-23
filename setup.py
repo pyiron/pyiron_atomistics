9b6d133c99a6b71e1235991019b9529d1eac8f2a
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-11-23 08:37:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         'phonopy==2.20.0',
         'pint==0.22',
         'pyiron_base==0.6.9',
+        'pylammpsmpi==0.2.6',
         'scipy==1.11.3',
         'seekpath==2.1.0',
         'scikit-learn==1.3.2',
