30eaaa348ed56d6f689ac069e1e6110e55892511
samwaseda <o.waseda@mpie.de>
2023-11-23 09:55:46 +0000

    fix typo

--- pyiron_atomistics/lammps/base.py
+++ pyiron_atomistics/lammps/base.py
@@ -207,7 +207,7 @@ class LammpsBase(AtomisticGenericJob):
     @property
     def potential(self):
         """
-        Execute view_potential() or list_potential() in order to see the pre-defined potential files
+        Execute view_potentials() or list_potentials() in order to see the pre-defined potential files
 
         Returns:
 
@@ -217,7 +217,7 @@ class LammpsBase(AtomisticGenericJob):
     @potential.setter
     def potential(self, potential_filename):
         """
-        Execute view_potential() or list_potential() in order to see the pre-defined potential files
+        Execute view_potentials() or list_potentials() in order to see the pre-defined potential files
 
         Args:
             potential_filename:
