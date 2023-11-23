55557571b72efdcb995bcb653b34989caa3c926d
samwaseda <o.waseda@mpie.de>
2023-11-23 10:11:14 +0000

    change location of warning

--- pyiron_atomistics/lammps/potential.py
+++ pyiron_atomistics/lammps/potential.py
@@ -238,6 +238,16 @@ class LammpsPotentialFile(PotentialAbstract):
             default_df=default_df,
             selected_atoms=selected_atoms,
         )
+        if len(self.list()) == 0:
+            state.logger.warning(
+                "It looks like your potential database is empty. In order to"
+                " install the standard pyiron library, run:\n\n"
+                "conda install -c conda-forge pyiron-data\n\n"
+                "Depending on the circumstances, you might have to change the"
+                " RESOURCE_PATHS of your .pyiron file. It is typically located in"
+                " your home directory. More can be found on the installation page"
+                " of the pyiron website."
+            )
 
     def default(self):
         if self._default_df is not None:
@@ -318,19 +328,8 @@ def view_potentials(structure: Atoms) -> pd.DataFrame:
     Returns:
         pandas.Dataframe: Dataframe including all potential parameters.
     """
-    lpf = LammpsPotentialFile()
-    if len(lpf.list()) == 0:
-        state.logger.warning(
-            "It looks like your potential database is empty. In order to"
-            " install the standard pyiron library, run:\n\n"
-            "conda install -c conda-forge pyiron-data\n\n"
-            "Depending on the circumstances, you might have to change the"
-            " RESOURCE_PATHS of your .pyiron file. It is typically located in"
-            " your home directory. More can be found on the installation page"
-            " of the pyiron website."
-        )
     list_of_elements = set(structure.get_chemical_symbols())
-    return lpf.find(list_of_elements)
+    return LammpsPotentialFile().find(list_of_elements)
 
 
 def list_potentials(structure: Atoms) -> List[str]:
