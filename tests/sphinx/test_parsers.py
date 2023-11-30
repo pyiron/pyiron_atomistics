9ecb56f74708604b44c6642c1535f2ce6371a199
samwaseda <o.waseda@mpie.de>
2023-11-30 08:38:21 +0000

    make a file for sphinx parsers

---
+++ tests/sphinx/test_parsers.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+
+import numpy as np
+import unittest
+from pathlib import Path
+
+from pyiron_atomistics.sphinx.output_parser import (
+    SphinxLogParser,
+    collect_energy_dat,
+    collect_residue_dat,
+    collect_spins_dat,
+    collect_relaxed_hist,
+    collect_energy_struct,
+    collect_eps_dat,
+)
+
+class TestSphinx(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.directory = Path(".").absolute() / "../static/sphinx"
+
+    def get_path(self, name):
+        return self.directory / f"{name}_hdf5" / f"{name}"
+
+    def test_energy(self):
+        E_dict = collect_energy_dat(
+            file_name="energy.dat",
+            cwd=self.get_path("sphinx_test_2_5")
+        )
+        self.assertTrue(
+            np.all(E_dict["scf_energy_free"][-1] < E_dict["scf_energy_int"][-1])
+        )
+        E_dict = collect_energy_dat(
+            file_name="energy.dat",
+            cwd=self.get_path("sphinx_test_2_3")
+        )
+        self.assertTrue("scf_energy_free" not in E_dict)
+
+
+if __name__ == "__main__":
+    unittest.main()
