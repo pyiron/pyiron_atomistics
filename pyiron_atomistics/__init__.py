bf73111042def0523c4ce911babcdcc02509fb64
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-12-04 16:40:24 +0100

    Update __init__.py

--- pyiron_atomistics/__init__.py
+++ pyiron_atomistics/__init__.py
@@ -1,6 +1,3 @@
-__version__ = "0.1"
-__all__ = []
-
 from pyiron_atomistics.project import Project
 from pyiron_atomistics.toolkit import AtomisticsTools
 from pyiron_atomistics.atomistics.structure.atoms import (
