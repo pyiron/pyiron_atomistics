b3ff2a1a09d431c4c3952644be2e32050f7a64fe
Jan Janssen <janssen@mpie.de>
2024-02-23 18:48:11 +0100

    Lammps: Fix final structure not being stored

--- pyiron_atomistics/lammps/base.py
+++ pyiron_atomistics/lammps/base.py
@@ -459,9 +459,11 @@ class LammpsBase(AtomisticGenericJob):
             ),
             group_name="output",
         )
-
-        if len(self.output.cells) > 0:
-            final_structure = self.get_structure(iteration_step=-1)
+        if len(hdf_dict['output/generic/cells']) > 0:
+            final_structure = self.structure.copy()
+            final_structure.indices = hdf_dict['output/generic/indices'][-1]
+            final_structure.positions = hdf_dict['output/generic/positions'][-1]
+            final_structure.cell = hdf_dict['output/generic/cells'][-1]
             if final_structure is not None:
                 hdf_dict.update(
                     {
