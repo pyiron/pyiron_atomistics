c98af6fcbbeeecfd7e05421863e89bc5115fe58a
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-03-26 17:19:10 -0500

    Update test_vasp.py

--- tests/vasp/test_vasp.py
+++ tests/vasp/test_vasp.py
@@ -365,6 +365,7 @@ class TestVasp(unittest.TestCase):
         job_chg_den = self.job_complete.restart_from_charge_density(job_name="chg")
         self.assertEqual(job_chg_den.structure, self.job_complete.get_structure(-1))
         print(posixpath.join(self.job_complete.working_directory, "CHGCAR"), job_chg_den.restart_file_list)
+        print([type(f) for f in self.job_complete.restart_file_list])
         self.assertTrue(
             posixpath.join(self.job_complete.working_directory, "CHGCAR")
             in job_chg_den.restart_file_list
