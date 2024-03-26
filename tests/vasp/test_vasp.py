29f8d936d37691c323d5cfea5c0eaf035bdad764
Jan Janssen <janssen@mpie.de>
2024-03-26 18:13:34 -0500

    fixes

--- tests/vasp/test_vasp.py
+++ tests/vasp/test_vasp.py
@@ -364,8 +364,6 @@ class TestVasp(unittest.TestCase):
             self.assertTrue(all([node in hdf_nodes for node in nodes]))
         job_chg_den = self.job_complete.restart_from_charge_density(job_name="chg")
         self.assertEqual(job_chg_den.structure, self.job_complete.get_structure(-1))
-        print(posixpath.join(self.job_complete.working_directory, "CHGCAR"), job_chg_den.restart_file_list)
-        print([type(f) for f in job_chg_den.restart_file_list])
         self.assertTrue(
             posixpath.join(self.job_complete.working_directory, "CHGCAR")
             in job_chg_den.restart_file_list
@@ -382,11 +380,11 @@ class TestVasp(unittest.TestCase):
         )
         self.assertEqual(job_chg_wave.structure, self.job_complete.get_structure(-1))
         self.assertTrue(
-            os.path.join(self.job_complete.working_directory, "WAVECAR")
+            posixpath.join(self.job_complete.working_directory, "WAVECAR")
             in job_chg_wave.restart_file_list
         )
         self.assertTrue(
-            os.path.join(self.job_complete.working_directory, "CHGCAR")
+            posixpath.join(self.job_complete.working_directory, "CHGCAR")
             in job_chg_wave.restart_file_list
         )
         for key, val in job_chg_wave.restart_file_dict.items():
