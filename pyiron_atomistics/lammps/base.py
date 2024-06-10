e09eca595719100380f4f3e3a101e8691e92345b
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-06-10 09:33:46 +0200

    Update base.py

--- pyiron_atomistics/lammps/base.py
+++ pyiron_atomistics/lammps/base.py
@@ -387,8 +387,8 @@ class LammpsBase(AtomisticGenericJob):
         self._set_selective_dynamics()
         if update_input_hdf5:
             self.input.to_hdf(self._hdf5)
-        if self.files is not None:
-            files_to_copy = {os.path.basename(f): f for f in self.input.potential.files if f is not None}
+        if self.input.potential.files is not None:
+            files_to_copy = {os.path.basename(f): f for f in self.input.potential.files}
         else:
             files_to_copy = {}
         return {
