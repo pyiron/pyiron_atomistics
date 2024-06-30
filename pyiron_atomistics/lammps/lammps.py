4c02664f0c05de88e8d020fa5a4fc79cf0004e81
Jan Janßen <janssen@mpie.de>
2024-06-30 18:34:09 +0200

    Add lammps_function()

--- pyiron_atomistics/lammps/lammps.py
+++ pyiron_atomistics/lammps/lammps.py
@@ -2,7 +2,10 @@
 # Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
+from pyiron_base import Project, ProjectHDFio
+
 from pyiron_atomistics.lammps.interactive import LammpsInteractive
+from pyiron_atomistics.atomistics.structure.atoms import ase_to_pyiron
 
 __author__ = "Joerg Neugebauer, Sudarsan Surendralal, Jan Janssen"
 __copyright__ = (
@@ -47,3 +50,32 @@ class Lammps(LammpsInteractive):
         super(Lammps, self).__init__(project, job_name)
 
         self._executable_activate(enforce=True)
+
+
+def lammps_function(working_directory, structure, potential, calc_mode="static", calc_kwargs={}, cutoff_radius=None,
+                    units="metal", bonds_kwargs={}, enable_h5md=False):
+    job = Lammps(
+        project=ProjectHDFio(project=Project(working_directory), file_name="lmp_funct_job", h5_path=None, mode=None),
+        job_name="lmp_funct_job")
+    job.structure = ase_to_pyiron(structure)
+    job.potential = potential
+    job.cutoff_radius = cutoff_radius
+    job.units = units
+    if calc_mode == "static":
+        job.calc_static()
+    elif calc_mode == "md":
+        job.calc_md(**calc_kwargs)
+    elif calc_mode == "minimize":
+        job.calc_minimize(**calc_kwargs)
+    elif calc_mode == "vcsgc":
+        job.calc_vcsgc(**calc_kwargs)
+    else:
+        raise ValueError()
+    if enable_h5md:
+        job.enable_h5md()
+    if len(bonds_kwargs) > 0:
+        job.define_bonds(**bonds_kwargs)
+
+    calculate_kwargs = job.calculate_kwargs
+    calculate_kwargs['working_directory'] = working_directory
+    return job.get_calculate_function(), calculate_kwargs
