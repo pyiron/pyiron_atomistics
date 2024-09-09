b8aa65797a82249ecfae1139c15f9f34c1891d64
samwaseda <o.waseda@mpie.de>
2024-09-09 17:40:15 +0000

    First attempt to separate sphinx input from pyiron

--- pyiron_atomistics/sphinx/spx_input.py
+++ pyiron_atomistics/sphinx/spx_input.py
@@ -1,14 +1,5 @@
-import posixpath
-from shutil import copyfile
-
 import numpy as np
-import scipy.constants
-from pyiron_base import DataContainer
 from typing import Optional
-
-BOHR_TO_ANGSTROM = (
-    scipy.constants.physical_constants["Bohr radius"][0] / scipy.constants.angstrom
-)
 
 
 def to_camel_case(snake_str):
@@ -22,158 +13,47 @@ def to_lower_camel_case(snake_str):
     return snake_str[0].lower() + camel_string[1:]
 
 
-class Group(DataContainer):
-    """
-    Dictionary-like object to store SPHInX inputs.
-
-    Attributes (sub-groups, parameters, & flags) can be set
-    and accessed via dot notation, or as standard dictionary
-    key/values.
-
-    `to_{job_type}` converts the Group to the format
-    expected by the given DFT code in its input files.
-    """
-
-    def to_sphinx(self, content="__self__", indent=0):
-        if content == "__self__":
-            content = self
-
-        def format_value(v):
-            if isinstance(v, bool):
-                return f" = {v};".lower()
-            elif isinstance(v, Group):
-                if len(v) == 0:
-                    return " {}"
-                else:
-                    return " {\n" + self.to_sphinx(v, indent + 1) + indent * "\t" + "}"
-            else:
-                if isinstance(v, np.ndarray):
-                    v = v.tolist()
-                return " = {!s};".format(v)
-
-        line = ""
-        for k, v in content.items():
-            if isinstance(v, Group) and len(v) > 0 and not v.has_keys():
-                for vv in v.values():
-                    line += indent * "\t" + str(k) + format_value(vv) + "\n"
-            else:
-                line += indent * "\t" + str(k) + format_value(v) + "\n"
-
-        return line
+def is_group(obj):
+    return isinstance(obj, dict) or (
+        isinstance(obj, list) and isinstance(obj[0], dict)
+    )
 
 
-def get_structure_group(
-    positions,
-    cell,
-    elements,
-    movable=None,
-    labels=None,
-    use_symmetry=True,
-    keep_angstrom=False,
-):
-    """
-    create a SPHInX Group object based on structure
-
-    Args:
-        positions ((n, 3)-list/numpy.ndarray): xyz-coordinates of the atoms
-        cell ((3, 3)-list/numpy.ndarray): Simulation box cdimensions
-        elements ((n,)-list/numpy.ndarray): Chemical symbols
-        movable (None/(n, 3)-list/nump.ndarray): Whether to fix the
-            movement of the atoms in given directions
-        labels (None/(n,)-list/numpy.ndarray): Extra labels to distinguish
-            atoms for symmetries (mainly for magnetic moments)
-        use_symmetry (bool): Whether or not consider internal symmetry
-        keep_angstrom (bool): Store distances in Angstroms or Bohr
-
-    Returns:
-        (Group): structure group
-    """
-    positions = np.array(positions)
-    cell = np.array(cell)
-    if not keep_angstrom:
-        cell /= BOHR_TO_ANGSTROM
-        positions /= BOHR_TO_ANGSTROM
-    structure_group = Group({"cell": np.array(cell)})
-    if movable is not None:
-        movable = np.array(movable)
+def format_value(v, indent=0):
+    if isinstance(v, bool):
+        return f" = {v};".lower()
+    elif is_group(v):
+        if len(v) == 0:
+            return " {}"
+        else:
+            return " {\n" + to_sphinx(v, indent + 1) + indent * "\t" + "}"
     else:
-        movable = np.full(shape=positions.shape, fill_value=True)
-    if positions.shape != movable.shape:
-        raise ValueError("positions.shape != movable.shape")
-    if labels is not None:
-        labels = np.array(labels)
-    else:
-        labels = np.full(shape=(len(positions),), fill_value=None)
-    if (len(positions),) != labels.shape:
-        raise ValueError("len(positions) != labels.shape")
-    species = structure_group.create_group("species")
-    for elm_species in np.unique(elements):
-        species.append(Group({"element": '"' + str(elm_species) + '"'}))
-        elm_list = elements == elm_species
-        atom_group = species[-1].create_group("atom")
-        for elm_pos, elm_magmom, selective in zip(
-            positions[elm_list],
-            labels[elm_list],
-            movable[elm_list],
-        ):
-            atom_group.append(Group())
-            if elm_magmom is not None:
-                atom_group[-1]["label"] = '"spin_' + str(elm_magmom) + '"'
-            atom_group[-1]["coords"] = np.array(elm_pos)
-            if all(selective):
-                atom_group[-1]["movable"] = True
-            elif any(selective):
-                for xx in np.array(["X", "Y", "Z"])[selective]:
-                    atom_group[-1]["movable" + xx] = True
-    if not use_symmetry:
-        structure_group.symmetry = Group(
-            {"operator": {"S": "[[1,0,0],[0,1,0],[0,0,1]]"}}
-        )
-    return structure_group
+        if isinstance(v, np.ndarray):
+            v = v.tolist()
+        return " = {!s};".format(v)
 
 
-def copy_potentials(origins, destinations):
-    """
-    Args:
-        origins (list): list of paths to potentials
-        destinations (list): list of paths to copy potentials to
-    """
-    for ori, des in zip(origins, destinations):
-        copyfile(ori, des)
-
-
-def write_spin_constraints(
-    file_name="spins.in", cwd=None, magmoms=None, constraints=None
-):
-    """
-    Write a text file containing a list of all spins named spins.in -
-    which is used for the external control scripts.
-
-    Args:
-        file_name (str): name of the file to be written (optional)
-        cwd (str): the current working directory (optinal)
-        spins_list (list): list of spins
-    """
-    for spin in magmoms:
-        if isinstance(spin, list) or isinstance(spin, np.ndarray):
-            raise ValueError("SPHInX only supports collinear spins at the moment.")
-    spins = np.array(magmoms).astype(str)
-    spins[~np.asarray(constraints)] = "X"
-    spins_str = "\n".join(spins) + "\n"
-    if cwd is not None:
-        file_name = posixpath.join(cwd, file_name)
-    with open(file_name, "w") as f:
-        f.write(spins_str)
+def to_sphinx(obj, indent=0):
+    line = ""
+    for k, v in obj.items():
+        if isinstance(v, list):
+            for vv in v:
+                line += indent * "\t" + k.split("___")[0] + format_value(vv, indent) + "\n"
+        else:
+            line += indent * "\t" + k.split("___")[0] + format_value(v, indent) + "\n"
+    return line
 
 
 def fill_values(group=None, **kwargs):
     if group is None:
-        group = Group()
+        group = {}
     for k, v in kwargs.items():
         if v is not None and v is not False:
-            if isinstance(v, Group):
-                group.create_group(k)
-            group[k] = v
+            if isinstance(v, list) and len(v) > 0 and isinstance(v[0], dict):
+                for i, vv in enumerate(v):
+                    group[f"{k}___{i}"] = vv
+            else:
+                group[k] = v
     return group
 
 
@@ -300,9 +180,9 @@ def get_scf_diag_group(
     d_spin_moment: Optional[float] = None,
     no_rho_storage: Optional[bool] = None,
     no_wave_storage: Optional[bool] = None,
-    CCG: Optional[Group] = None,
-    block_CCG: Optional[Group] = None,
-    preconditioner: Optional[Group] = None,
+    CCG: Optional[dict] = None,
+    block_CCG: Optional[dict] = None,
+    preconditioner: Optional[dict] = None,
 ):
     """
     Args:
@@ -322,9 +202,9 @@ def get_scf_diag_group(
         d_spin_moment (float): Spin moment convergence criterion
         no_rho_storage (bool): Do not store density
         no_wave_storage (bool): Do not store wave functions
-        CCG (Group): Conjugate gradient method
-        block_CCG (Group): Block conjugate gradient method
-        preconditioner (Group): Preconditioner
+        CCG (dict): Conjugate gradient method
+        block_CCG (dict): Block conjugate gradient method
+        preconditioner (dict): Preconditioner
     """
     return fill_values(
         dEnergy=d_energy,
@@ -350,11 +230,11 @@ def get_scf_diag_group(
 
 
 def get_born_oppenheimer_group(
-    scf_diag: Optional[Group] = None,
-)
+    scf_diag: Optional[dict] = None,
+):
     """
     Args:
-        scf_diag (Group): SCF diagonalization
+        scf_diag (dict): SCF diagonalization
     """
     return fill_values(scfDiag=scf_diag)
 
@@ -367,7 +247,7 @@ def get_QN_group(
     max_step_length: Optional[float] = None,
     hessian: Optional[str] = None,
     drift_filter: Optional[bool] = None,
-    born_oppenheimer: Optional[Group] = None,
+    born_oppenheimer: Optional[dict] = None,
 ):
     """
     Args:
@@ -378,7 +258,7 @@ def get_QN_group(
         max_step_length (float): Maximum step length
         hessian (str): Initialize Hessian from file
         drift_filter (bool): Drift filter
-        born_oppenheimer (Group): Born-Oppenheimer
+        born_oppenheimer (dict): Born-Oppenheimer
     """
     return fill_values(
         maxSteps=max_steps,
@@ -401,7 +281,7 @@ def get_linQN_group(
     n_projectors: Optional[int] = None,
     hessian: Optional[str] = None,
     drift_filter: Optional[bool] = None,
-    born_oppenheimer: Optional[Group] = None,
+    born_oppenheimer: Optional[dict] = None,
 ):
     """
     Args:
@@ -413,7 +293,7 @@ def get_linQN_group(
         n_projectors (int): Number of projectors
         hessian (str): Initialize Hessian from file
         drift_filter (bool): Drift filter
-        born_oppenheimer (Group): Born-Oppenheimer
+        born_oppenheimer (dict): Born-Oppenheimer
     """
     return fill_values(
         maxSteps=max_steps,
@@ -437,7 +317,7 @@ def get_ricQN_group(
     n_projectors: Optional[int] = None,
     soft_mode_damping: Optional[float] = None,
     drift_filter: Optional[bool] = None,
-    born_oppenheimer: Optional[Group] = None,
+    born_oppenheimer: Optional[dict] = None,
 ):
     """
     Args:
@@ -449,7 +329,7 @@ def get_ricQN_group(
         n_projectors (int): Number of projectors
         soft_mode_damping (float): Soft mode damping
         drift_filter (bool): Drift filter
-        born_oppenheimer (Group): Born-Oppenheimer
+        born_oppenheimer (dict): Born-Oppenheimer
     """
     return fill_values(
         maxSteps=max_steps,
@@ -471,7 +351,7 @@ def get_ric_group(
     plane_cut_limit: Optional[float] = None,
     with_angles: Optional[bool] = None,
     bvk_atoms: Optional[str] = None,
-    born_oppenheimer: Optional[Group] = None,
+    born_oppenheimer: Optional[dict] = None,
 ):
     """
     Args:
@@ -485,7 +365,7 @@ def get_ric_group(
             comma-separated list must be enclosed by square brackets []. This
             adds a bond-directional coordinate to each bond of the atoms in the
             list.
-        born_oppenheimer (Group): Born-Oppenheimer
+        born_oppenheimer (dict): Born-Oppenheimer
     """
     return fill_values(
         maxDist=max_dist,
@@ -510,7 +390,7 @@ def get_ricTS_group(
     max_dir_rot: Optional[float] = None,
     scheme: Optional[int] = None,
     drift_filter: Optional[bool] = None,
-    born_oppenheimer: Optional[Group] = None,
+    born_oppenheimer: Optional[dict] = None,
 ):
     """
     Args:
@@ -525,7 +405,7 @@ def get_ricTS_group(
         max_dir_rot (float): Maximum direction rotation
         scheme (int): Scheme
         drift_filter (bool): Drift filter
-        born_oppenheimer (Group): Born-Oppenheimer
+        born_oppenheimer (dict): Born-Oppenheimer
     """
     return fill_values(
         maxSteps=max_steps,
@@ -533,7 +413,7 @@ def get_ricTS_group(
         dF=dF,
         dEnergy=d_energy,
         nProjectors=n_projectors,
-        maxStepLength=max_step
+        maxStepLength=max_step_length,
         transCurvature=trans_curvature,
         anyStationaryPoint=any_stationary_point,
         maxDirRot=max_dir_rot,
@@ -541,4 +421,3 @@ def get_ricTS_group(
         driftFilter=drift_filter,
         bornOppenheimer=born_oppenheimer,
     )
-
