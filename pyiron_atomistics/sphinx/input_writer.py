9670acbfb76ff044460bc619e79d70d8d8460183
samwaseda <o.waseda@mpie.de>
2024-09-10 07:34:59 +0000

    remove remaining stuff

--- pyiron_atomistics/sphinx/input_writer.py
+++ pyiron_atomistics/sphinx/input_writer.py
@@ -4,22 +4,10 @@ from shutil import copyfile
 import numpy as np
 import scipy.constants
 from pyiron_base import DataContainer
-from typing import Optional
 
 BOHR_TO_ANGSTROM = (
     scipy.constants.physical_constants["Bohr radius"][0] / scipy.constants.angstrom
 )
-
-
-def to_camel_case(snake_str):
-    return "".join(x.capitalize() for x in snake_str.lower().split("_"))
-
-
-def to_lower_camel_case(snake_str):
-    # We capitalize the first letter of each component except the first one
-    # with the 'capitalize' method and join them together.
-    camel_string = to_camel_case(snake_str)
-    return snake_str[0].lower() + camel_string[1:]
 
 
 class Group(DataContainer):
