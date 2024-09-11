39447988b9a806c16b09c6a2b2578110ae6f6b01
samwaseda <o.waseda@mpie.de>
2024-09-11 14:08:03 +0000

    update tests

--- tests/sphinx/test_input_functions.py
+++ tests/sphinx/test_input_functions.py
@@ -7,27 +7,30 @@ from pyiron_atomistics.sphinx import input_functions as spx_input
 
 
 class TestSphinx(unittest.TestCase):
-    def test_main(self):
-
+    def test_loop(self):
         scf_ccg = [
             spx_input.get_scf_block_CCG_group(d_energy=1 / (1 + i))
-            for i in range(3)
+            for i in range(2)
         ]
         born_oppenheimer = spx_input.get_scf_diag_group(
             d_energy=1e-6, block_CCG=scf_ccg
         )
+        self.assertEqual(
+            spx_input.to_sphinx(born_oppenheimer),
+            "dEnergy = 1e-06;\nblockCCG {\n\tdEnergy = 1.0;\n}\nblockCCG {\n\tdEnergy = 0.5;\n}\n"
+        )
+
+    def test_multiple_keys(self):
         main = spx_input.get_main_group(
-            scfDiag=spx_input.get_scf_diag_group(max_steps=10),
-            QN=spx_input.get_QN_group(
-                max_steps=100, born_oppenheimer=born_oppenheimer
-            ),
-            linQN=spx_input.get_linQN_group(max_steps=10, dF=1.0e-4),
-            QN_=spx_input.get_QN_group(dX=1.0e-2)
+            scfDiag=spx_input.get_scf_diag_group(),
+            QN=spx_input.get_QN_group(),
+            linQN=spx_input.get_linQN_group(),
+            QN_=spx_input.get_QN_group()
         )
         inp_all = spx_input.get_all_group(main=main)
         self.assertEqual(
             spx_input.to_sphinx(inp_all),
-            "main {\n\tscfDiag {\n\t\tmaxSteps = 10;\n\t}\n\tQN {\n\t\tmaxSteps = 100;\n\t\tbornOppenheimer {\n\t\t\tdEnergy = 1e-06;\n\t\t\tblockCCG {\n\t\t\t\tdEnergy = 1.0;\n\t\t\t}\n\t\t\tblockCCG {\n\t\t\t\tdEnergy = 0.5;\n\t\t\t}\n\t\t\tblockCCG {\n\t\t\t\tdEnergy = 0.3333333333333333;\n\t\t\t}\n\t\t}\n\t}\n\tlinQN {\n\t\tmaxSteps = 10;\n\t\tdF = 0.0001;\n\t}\n\tQN {\n\t\tdX = 0.01;\n\t}\n}\n"
+            "main {\n\tscfDiag {}\n\tQN {}\n\tlinQN {}\n\tQN {}\n}\n"
         )
 
 
