91b5d0f1e6717726556ce731b9a88d5a1848e14d
Jan Janßen <janssen@mpie.de>
2025-02-27 16:52:22 +0100

    fix electronic structure

--- pyiron_atomistics/dft/waves/electronic.py
+++ pyiron_atomistics/dft/waves/electronic.py
@@ -35,3 +35,74 @@ def electronic_structure_dict_to_hdf(data_dict, hdf, group_name):
             structure_dict_to_hdf(data_dict=data_dict["structure"], hdf=h_es)
 
         dict_group_to_hdf(data_dict=data_dict, hdf=h_es, group="dos")
+
+
+def from_hdf(es, hdf, group_name="electronic_structure"):
+    """
+    Retrieve the object from the hdf5 file
+    Args:
+        hdf: Path to the hdf5 file/group in the file
+        group_name: Name of the group under which the attributes are stored
+    """
+    if "dos" not in hdf[group_name].list_groups():
+        from_hdf_old(es=es, hdf=hdf, group_name=group_name)
+    else:
+        with hdf.open(group_name) as h_es:
+            if "TYPE" not in h_es.list_nodes():
+                h_es["TYPE"] = str(type(es))
+            nodes = h_es.list_nodes()
+            if es.structure is not None:
+                es.structure.to_hdf(h_es)
+            es.kpoint_list = h_es["k_points"]
+            es.kpoint_weights = h_es["k_weights"]
+            if len(h_es["eig_matrix"].shape) == 2:
+                es.eigenvalue_matrix = np.array([h_es["eig_matrix"]])
+                es.occupancy_matrix = np.array([h_es["occ_matrix"]])
+            else:
+                es._eigenvalue_matrix = h_es["eig_matrix"]
+                es._occupancy_matrix = h_es["occ_matrix"]
+            es.n_spins = len(es._eigenvalue_matrix)
+            if "efermi" in nodes:
+                es.efermi = h_es["efermi"]
+            with h_es.open("dos") as h_dos:
+                nodes = h_dos.list_nodes()
+                es.dos_energies = h_dos["energies"]
+                es.dos_densities = h_dos["tot_densities"]
+                es.dos_idensities = h_dos["int_densities"]
+                if "grand_dos_matrix" in nodes:
+                    es.grand_dos_matrix = h_dos["grand_dos_matrix"]
+                if "resolved_densities" in nodes:
+                    es.resolved_densities = h_dos["resolved_densities"]
+            es._output_dict = h_es.copy()
+        es.generate_from_matrices()
+
+
+def from_hdf_old(es, hdf, group_name="electronic_structure"):
+    """
+    Retrieve the object from the hdf5 file
+    Args:
+        hdf: Path to the hdf5 file/group in the file
+        group_name: Name of the group under which the attributes are stored
+    """
+    with hdf.open(group_name) as h_es:
+        if "structure" in h_es.list_nodes():
+            es.structure = Atoms().from_hdf(h_es)
+        nodes = h_es.list_nodes()
+        es.kpoint_list = h_es["k_points"]
+        es.kpoint_weights = h_es["k_point_weights"]
+        es.eigenvalue_matrix = np.array([h_es["eigenvalue_matrix"]])
+        es.occupancy_matrix = np.array([h_es["occupancy_matrix"]])
+        try:
+            es.dos_energies = h_es["dos_energies"]
+            es.dos_densities = h_es["dos_densities"]
+            es.dos_idensities = h_es["dos_idensities"]
+        except ValueError:
+            pass
+        if "fermi_level" in nodes:
+            es.efermi = h_es["fermi_level"]
+        if "grand_dos_matrix" in nodes:
+            es.grand_dos_matrix = h_es["grand_dos_matrix"]
+        if "resolved_densities" in nodes:
+            es.resolved_densities = h_es["resolved_densities"]
+        es._output_dict = h_es.copy()
+    es.generate_from_matrices()
--- tests/dft/waves/test_electronic.py
+++ tests/dft/waves/test_electronic.py
@@ -8,6 +8,7 @@ import posixpath
 import numpy as np
 
 from pyiron_atomistics.atomistics.structure.atoms import Atoms
+from pyiron_atomistics.dft.waves.electronic import from_hdf_old, from_hdf, electronic_structure_dict_to_hdf
 from pyiron_vasp.vasp.vasprun import Vasprun
 from pyiron_vasp.dft.waves.dos import Dos
 from pyiron_vasp.dft.waves.electronic import ElectronicStructure
@@ -90,9 +91,9 @@ class TestElectronicStructure(unittest.TestCase):
         abs_filename = os.path.abspath(filename)
         hdf_obj = FileHDFio(abs_filename)
         es_obj_old = ElectronicStructure()
-        es_obj_old.from_hdf_old(hdf_obj, "es_old")
+        from_hdf_old(es=es_obj_old, hdf=hdf_obj, group_name="es_old")
         es_obj_new = ElectronicStructure()
-        es_obj_new.from_hdf(hdf=hdf_obj, group_name="es_new")
+        from_hdf(es=es_obj_new, hdf=hdf_obj, group_name="es_new")
         self.assertEqual(es_obj_old.efermi, es_obj_new.efermi)
         self.assertEqual(es_obj_old.is_metal, es_obj_new.is_metal)
         self.assertEqual(es_obj_old.vbm, es_obj_new.vbm)
@@ -109,9 +110,9 @@ class TestElectronicStructure(unittest.TestCase):
         abs_filename = os.path.abspath(filename)
         hdf_obj = FileHDFio(abs_filename)
         es_obj_old = self.es_list[1]
-        es_obj_old.to_hdf(hdf_obj, group_name="written_es")
+        electronic_structure_dict_to_hdf(data_dict=es_obj_old.to_dict(), hdf=hdf_obj, group_name="written_es")
         es_obj_new = ElectronicStructure()
-        es_obj_new.from_hdf(hdf=hdf_obj, group_name="written_es")
+        from_hdf(es=es_obj_new, hdf=hdf_obj, group_name="written_es")
         self.assertTrue(
             np.array_equal(
                 hdf_obj["written_es/dos/grand_dos_matrix"],
