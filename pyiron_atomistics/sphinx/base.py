1f77345d6606bfa2cac331b986829e92136900b2
pyiron-runner <pyiron@mpie.de>
2025-07-08 20:21:31 +0000

    Format black

--- pyiron_atomistics/sphinx/base.py
+++ pyiron_atomistics/sphinx/base.py
@@ -1969,9 +1969,9 @@ class Output:
         """
         try:
             results = collect_eps_dat(
-                file_name=os.path.join(cwd, file_name)
-                if file_name is not None
-                else None,
+                file_name=(
+                    os.path.join(cwd, file_name) if file_name is not None else None
+                ),
                 spins=self._job._spin_enabled,
             )
             for k, v in results.items():
